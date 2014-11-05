5e899f40d9cd2ac1d7ffd99c7211f70d3fb5433e
James Mac Redmond <james@learnsignal.com>
2014-11-05 14:28:47 +0000

    Ran migration for development and test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104145851) do
+ActiveRecord::Schema.define(version: 20141105141747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 20141104145851) do
     t.boolean  "is_cpd",                            default: false, null: false
     t.integer  "sorting_order"
     t.boolean  "active",                            default: false, null: false
+    t.float    "best_possible_first_attempt_score"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "exam_sections", force: true do |t|
+    t.string   "name"
+    t.string   "name_url"
+    t.integer  "exam_level_id"
+    t.boolean  "active",                            default: false, null: false
+    t.integer  "sorting_order"
     t.float    "best_possible_first_attempt_score"
     t.datetime "created_at"
     t.datetime "updated_at"
