fcae22000e373bb1c17db7a6b6cce8abee60b584
James Mac Redmond <james@learnsignal.com>
2014-11-05 16:09:12 +0000

    Changes to db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,27 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104154759) do
+ActiveRecord::Schema.define(version: 20141105141747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "course_modules", force: true do |t|
-    t.integer  "institution_id"
-    t.integer  "qualification_id"
-    t.integer  "exam_level_id"
-    t.integer  "exam_section_id"
-    t.string   "name"
-    t.string   "name_url"
-    t.text     "description"
-    t.integer  "tutor_id"
-    t.integer  "sorting_order"
-    t.integer  "estimated_time_in_seconds"
-    t.boolean  "compulsory",                default: false, null: false
-    t.boolean  "active",                    default: false, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "exam_levels", force: true do |t|
     t.integer  "qualification_id"
@@ -40,6 +23,17 @@ ActiveRecord::Schema.define(version: 20141104154759) do
     t.boolean  "is_cpd",                            default: false, null: false
     t.integer  "sorting_order"
     t.boolean  "active",                            default: false, null: false
+    t.float    "best_possible_first_attempt_score"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "exam_sections", force: true do |t|
+    t.string   "name"
+    t.string   "name_url"
+    t.integer  "exam_level_id"
+    t.boolean  "active",                            default: false, null: false
+    t.integer  "sorting_order"
     t.float    "best_possible_first_attempt_score"
     t.datetime "created_at"
     t.datetime "updated_at"
