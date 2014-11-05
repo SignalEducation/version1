ddb9fc247c7fda66a45bc460281033ee116c1a39
James Mac Redmond <james@learnsignal.com>
2014-11-05 15:43:19 +0000

    Updated the exam_sections factory and added the flash messages translations to the localisation file

--- spec/factories/exam_sections.rb
+++ spec/factories/exam_sections.rb
@@ -1,11 +1,33 @@
+# == Schema Information
+#
+# Table name: exam_sections
+#
+#  id                                :integer          not null, primary key
+#  name                              :string(255)
+#  name_url                          :string(255)
+#  exam_level_id                     :integer
+#  active                            :boolean          default(FALSE), not null
+#  sorting_order                     :integer
+#  best_possible_first_attempt_score :float
+#  created_at                        :datetime
+#  updated_at                        :datetime
+#
+
 FactoryGirl.define do
   factory :exam_section do
-    name "MyString"
-name_url "MyString"
-exam_level_id 1
-active false
-sorting_order 1
-best_possible_first_attempt_score 1.5
+    sequence(:name)       {|n| "Exam Level #{n}"}
+    sequence(:name_url)   {|n| "exam-level-#{n}"}
+    exam_level_id          { ExamLevel.first.try(:id) || 1 }
+    sequence(:sorting_order) {|n| n * 10}
+    best_possible_first_attempt_score 1.5
+
+    factory :active_institution do
+      active              true
+    end
+
+    factory :inactive_institution do
+      active              false
+    end
   end
 
 end
