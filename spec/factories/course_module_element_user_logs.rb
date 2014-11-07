29bceb0b0ec84fbc687322e0bd930885c0f36881
James Mac Redmond <james@learnsignal.com>
2014-11-07 17:34:09 +0000

    Finished #69 - Course Module Element User Log

---
+++ spec/factories/course_module_element_user_logs.rb
@@ -0,0 +1,38 @@
+# == Schema Information
+#
+# Table name: course_module_element_user_logs
+#
+#  id                       :integer          not null, primary key
+#  course_module_element_id :integer
+#  user_id                  :integer
+#  session_guid             :string(255)
+#  element_completed        :boolean          default(FALSE), not null
+#  time_taken_in_seconds    :integer
+#  quiz_score_actual        :integer
+#  quiz_score_potential     :integer
+#  is_video                 :boolean          default(FALSE), not null
+#  is_quiz                  :boolean          default(FALSE), not null
+#  course_module_id         :integer
+#  latest_attempt           :boolean          default(TRUE), not null
+#  corporate_customer_id    :integer
+#  created_at               :datetime
+#  updated_at               :datetime
+#
+
+FactoryGirl.define do
+  factory :course_module_element_user_log do
+    course_module_element_id 1
+    user_id 1
+    session_guid "MyString"
+    element_completed false
+    time_taken_in_seconds 1
+    quiz_score_actual 1
+    quiz_score_potential 1
+    is_video false
+    is_quiz false
+    course_module_id 1
+    latest_attempt false
+    corporate_customer_id 1
+  end
+
+end
