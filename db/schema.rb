65957daccce7d89fabc6a9f3197aa81410462dce
James Mac Redmond <james@learnsignal.com>
2014-11-11 13:36:05 +0000

    Executed generator, fixed factory file indentation, edited the migration file and ran the migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111091849) do
+ActiveRecord::Schema.define(version: 20141111132957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -222,6 +222,16 @@ ActiveRecord::Schema.define(version: 20141111091849) do
     t.string   "difficulty_level"
     t.text     "solution_to_the_question"
     t.text     "hints"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "student_exam_tracks", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "exam_level_id"
+    t.integer  "exam_section_id"
+    t.integer  "latest_course_module_element_id"
+    t.integer  "exam_schedule_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
