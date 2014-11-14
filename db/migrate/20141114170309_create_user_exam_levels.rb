949871548446fa3121db7462068ff0870b8fc83b
James Mac Redmond <james@learnsignal.com>
2014-11-14 17:07:03 +0000

    Fixed indentation in factory file and added 'index: true' to all fields in migration file

--- db/migrate/20141114170309_create_user_exam_levels.rb
+++ db/migrate/20141114170309_create_user_exam_levels.rb
@@ -1,9 +1,9 @@
 class CreateUserExamLevels < ActiveRecord::Migration
   def change
     create_table :user_exam_levels do |t|
-      t.integer :user_id
-      t.integer :exam_level_id
-      t.integer :exam_schedule_id
+      t.integer :user_id, index: true
+      t.integer :exam_level_id, index: true
+      t.integer :exam_schedule_id, index: true
 
       t.timestamps
     end
--- spec/factories/user_exam_levels.rb
+++ spec/factories/user_exam_levels.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user_exam_level do
     user_id 1
-exam_level_id 1
-exam_schedule_id 1
+    exam_level_id 1
+    exam_schedule_id 1
   end
 
 end
