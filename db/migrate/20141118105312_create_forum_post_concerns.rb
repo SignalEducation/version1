e46acc22859fd195b271503f2ff356299803adee
James Mac Redmond <james@learnsignal.com>
2014-11-18 10:56:03 +0000

    Added to fields in the migration file and fixed indentation in the factory file

--- db/migrate/20141118105312_create_forum_post_concerns.rb
+++ db/migrate/20141118105312_create_forum_post_concerns.rb
@@ -1,10 +1,10 @@
 class CreateForumPostConcerns < ActiveRecord::Migration
   def change
     create_table :forum_post_concerns do |t|
-      t.integer :forum_post_id
-      t.integer :user_id
+      t.integer :forum_post_id, index: true
+      t.integer :user_id, index: true
       t.string :reason
-      t.boolean :live
+      t.boolean :live, default: true, null: false
 
       t.timestamps
     end
