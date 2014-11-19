549ba22bf149851a60b0aef0a9b1218bd9a833ff
James Mac Redmond <james@learnsignal.com>
2014-11-19 17:38:13 +0000

    X'd out the request test and made changes to the factory file

--- spec/factories/vat_codes.rb
+++ spec/factories/vat_codes.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: vat_codes
+#
+#  id         :integer          not null, primary key
+#  country_id :integer
+#  name       :string(255)
+#  label      :string(255)
+#  wiki_url   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 FactoryGirl.define do
   factory :vat_code do
     country_id 1
