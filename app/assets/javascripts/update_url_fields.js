715fd0befe1694c2a16a8f7bb89e4d4ef866b625
danlaffan <dan.laffan@mac.com>
2014-12-13 17:14:56 +0000

    re#177 added JS to auto-populate name_url

--- app/assets/javascripts/update_url_fields.js
+++ app/assets/javascripts/update_url_fields.js
@@ -1,11 +1,15 @@
 $(document).on('ready', function() {
 
-  $('.update-my-url').on('change blur', function() {
+  $('.update-my-url').on('change', function() {
     var the_id = '#' +$(this).attr('id') + '_url';
-    if ($(the_id).val() == '') {
+    if ($(the_id).val() == "") {
       $(the_id).val($(this).val().replace(/ |\//g, '-').toLowerCase());
     } else {
-      $(the_id).parent().addClass('');
+      if ($(the_id).parent().hasClass('has-warning')) {} else {
+        $(the_id).parent().addClass('has-warning');
+        $(the_id).attr('aria-describedby', 'helpBlock');
+        $(the_id).parent().append("<span id='helpBlock' class='help-block'>Do you need to update the URL?</span>")
+      }
     }
   })
 
