ec78c9c6ef6662974ccd1977f1d151c4a54b5038
danlaffan <dan.laffan@mac.com>
2014-12-15 10:34:56 +0000

    re#177 extended the descriptions for degree_of_wrongness

--- app/models/quiz_answer.rb
+++ app/models/quiz_answer.rb
@@ -18,7 +18,7 @@ class QuizAnswer < ActiveRecord::Base
   attr_accessible :quiz_question_id, :degree_of_wrongness, :wrong_answer_explanation_text, :wrong_answer_video_id, :quiz_contents_attributes
 
   # Constants
-  WRONGNESS = %w(correct slight medium very)
+  WRONGNESS = ['correct', 'slightly wrong', 'wrong', 'very wrong']
 
   # relationships
   has_many :quiz_attempts
