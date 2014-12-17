ecf97b677bcc190209b2f0382f3682a1464e799b
James Mac Redmond <james@learnsignal.com>
2014-12-17 10:03:33 +0000

    Ran migrations and fixed indentation in the factory file

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -1,3 +1,34 @@
+# == Schema Information
+#
+# Table name: static_pages
+#
+#  id                         :integer          not null, primary key
+#  name                       :string(255)
+#  publish_from               :datetime
+#  publish_to                 :datetime
+#  allow_multiples            :boolean          default(FALSE), not null
+#  public_url                 :string(255)
+#  use_standard_page_template :boolean          default(FALSE), not null
+#  head_content               :text
+#  body_content               :text
+#  created_by                 :integer
+#  updated_by                 :integer
+#  add_to_navbar              :boolean          default(FALSE), not null
+#  add_to_footer              :boolean          default(FALSE), not null
+#  menu_label                 :string(255)
+#  tooltip_text               :string(255)
+#  language                   :string(255)
+#  mark_as_noindex            :boolean          default(FALSE), not null
+#  mark_as_nofollow           :boolean          default(FALSE), not null
+#  seo_title                  :string(255)
+#  seo_description            :string(255)
+#  approved_country_ids       :text
+#  default_page_for_this_url  :boolean          default(FALSE), not null
+#  make_this_page_sticky      :boolean          default(FALSE), not null
+#  created_at                 :datetime
+#  updated_at                 :datetime
+#
+
 class StaticPage < ActiveRecord::Base
 
   # attr-accessible
