d4a7bf815be88d60b312ceaf35c3949391910384
danlaffan <dan.laffan@mac.com>
2014-12-17 16:40:47 +0000

    re#134 re#141 further WIP - reformatted the breadcrumbs

--- app/controllers/course_modules_controller.rb
+++ app/controllers/course_modules_controller.rb
@@ -7,7 +7,7 @@ class CourseModulesController < ApplicationController
   before_action :get_variables, except: :show
 
   def index
-    qualification = Qualification.where(name_url: params[:qualification_url].to_s).first || Qualification.all_in_order.first
+    qualification = Qualification.where(name_url: params[:qualification_url].to_s).first || ExamLevel.all_in_order.first.qualification
     exam_level = qualification.try(:exam_levels).try(:first)
     if exam_level
       redirect_to course_module_special_link(exam_level)
