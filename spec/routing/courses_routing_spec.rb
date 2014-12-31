6434170016e67aaa221ed06590b80d4bc48ebfb6
danlaffan <dan.laffan@mac.com>
2014-12-31 16:14:00 +0000

    re#134 fixed some failing tests

--- spec/routing/courses_routing_spec.rb
+++ spec/routing/courses_routing_spec.rb
@@ -10,11 +10,11 @@ RSpec.describe CoursesController, type: :routing do
     # special routes
 
     it 'routes to #show' do
-      expect(get: '/courses/subject_area/institution/exam_level/exam_section/course_module').to route_to('courses#show', subject_area_name_url: 'subject_area', institution_name_url: 'institution', exam_level_name_url: 'exam_level', exam_section_name_url: 'exam_section', course_module_name_url: 'course_module')
+      expect(get: '/courses/subject_area/institution/qualification/exam_level/exam_section/course_module').to route_to('courses#show', subject_area_name_url: 'subject_area', institution_name_url: 'institution', qualification_name_url: 'qualification', exam_level_name_url: 'exam_level', exam_section_name_url: 'exam_section', course_module_name_url: 'course_module')
     end
 
     it 'routes to #show' do
-      expect(get: '/courses/subject_area/institution/exam_level/exam_section/course_module/course_module_element').to route_to('courses#show', subject_area_name_url: 'subject_area', institution_name_url: 'institution', exam_level_name_url: 'exam_level', exam_section_name_url: 'exam_section', course_module_name_url: 'course_module', course_module_element_name_url: 'course_module_element')
+      expect(get: '/courses/subject_area/institution/qualification/exam_level/exam_section/course_module/course_module_element').to route_to('courses#show', subject_area_name_url: 'subject_area', institution_name_url: 'institution', qualification_name_url: 'qualification', exam_level_name_url: 'exam_level', exam_section_name_url: 'exam_section', course_module_name_url: 'course_module', course_module_element_name_url: 'course_module_element')
     end
 
   end
--- spec/views/exam_sections/index.html.haml_spec.rb
+++ spec/views/exam_sections/index.html.haml_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'exam_sections/index', type: :view do
     allow(view).to receive(:tick_or_cross).and_return('nice_boolean')
     @qualification = FactoryGirl.create(:qualification)
     @exam_level = FactoryGirl.create(:exam_level, qualification_id: @qualification.id)
+    @exam_levels = ExamLevel.all_in_order
     temp_exam_sections = FactoryGirl.create_list(:exam_section, 2, exam_level_id: @exam_level.id)
     @exam_sections = ExamSection.paginate(page: 1, per_page: 10)
   end
