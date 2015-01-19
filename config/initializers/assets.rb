5514c2fc205f3ffa3d7bc43cfdd819d9edf0194e
Dan Laffan <dan@learnsignal.com>
2015-01-19 12:03:22 +0000

    re#184 fixed final bug in assets precompile processing on staging/production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Added at the suggestion of
 # http://stackoverflow.com/questions/18294150/how-to-use-fonts-in-rails-4
-Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf)
+# and
+# http://stackoverflow.com/questions/10905905/using-fonts-with-rails-asset-pipeline
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
