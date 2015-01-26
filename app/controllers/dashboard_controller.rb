660fc89f192935ca8d186892474ca9ddeec3c2b1
James Mac Redmond <james@learnsignal.com>
2015-01-26 15:55:57 +0000

    Added the content_manager dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,8 +6,8 @@ class DashboardController < ApplicationController
     if current_user.nil? || current_user.individual_student? || current_user.admin?
       @dashboard_type << 'individual_student'
     end
-    @dashboard_type << 'tutor' if current_user.try(:tutor?)
     @dashboard_type << 'admin' if current_user.try(:admin?)
+    @dashboard_type << 'tutor' if current_user.try(:tutor?)
     @dashboard_type << 'content_manager' if current_user.try(:content_manager?)
 
     # @dashboard_type could == ['blogger','tutor','admin']
@@ -19,24 +19,33 @@ class DashboardController < ApplicationController
       @course_modules = CourseModule.where(tutor_id: current_user.id)
     end
 
+    if @dashboard_type.include?('content_manager')
+      @static_pages = StaticPage.all_in_order
+      @edited_recently = @static_pages.where('updated_at > ?', Proc.new{Time.now - 3.days}.call)
+      @recently_published = @static_pages.where('publish_from > ?', Time.now - 3.days)
+      @publishes_soon = @static_pages.where('publish_from > ? AND publish_from < ?', Proc.new{Time.now}.call, Proc.new{Time.now + 3.days}.call)
+      @recently_expired = @static_pages.where('publish_to IS NOT NULL AND publish_to < ? AND publish_to > ?', Proc.new{Time.now}.call, Proc.new{Time.now - 3.days}.call)
+      @expires_soon = @static_pages.where('publish_to > ? AND publish_to < ?', Proc.new{Time.now}.call, Proc.new{Time.now + 3.days}.call)
+    end
+
     if @dashboard_type.include?('admin')
       @all_users = User.all
       @active_users = @all_users.where(active: true)
-      @new_users = @all_users.where('created_at > ?', Time.now - 7.days)
+      @new_users = @all_users.where('created_at > ?', Proc.new{Time.now - 7.days}.call)
       @all_user_groups = UserGroup.all
-      @new_user_groups = @all_user_groups.where('created_at > ?', Time.now - 7.days)
+      @new_user_groups = @all_user_groups.where('created_at > ?', Proc.new{Time.now - 7.days}.call)
       @all_subject_areas = SubjectArea.all
       @active_subject_areas = @all_subject_areas.where(active: true)
-      @new_subject_areas = @all_subject_areas.where('created_at > ?', Time.now - 7.days)
+      @new_subject_areas = @all_subject_areas.where('created_at > ?', Proc.new{Time.now - 7.days}.call)
       @all_institutions = Institution.all
       @active_institutions = @all_institutions.where(active: true)
-      @new_institutions = @all_institutions.where('created_at > ?', Time.now - 7.days)
+      @new_institutions = @all_institutions.where('created_at > ?', Proc.new{Time.now - 7.days}.call)
       @all_qualifications = Qualification.all
       @active_qualifications = @all_qualifications.where(active: true)
-      @new_qualifications = @all_qualifications.where('created_at > ?', Time.now - 7.days)
+      @new_qualifications = @all_qualifications.where('created_at > ?', Proc.new{Time.now - 7.days}.call)
       @all_exam_levels = ExamLevel.all
       @active_exam_levels = @all_exam_levels.where(active: true)
-      @new_exam_levels = @all_exam_levels.where('created_at > ?', Time.now - 7.days)
+      @new_exam_levels = @all_exam_levels.where('created_at > ?', Proc.new{Time.now - 7.days}.call)
     end
 
   end
