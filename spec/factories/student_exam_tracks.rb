07ca6827638442adc30f701306404bc8f41d64e7
James Mac Redmond <james@learnsignal.com>
2015-01-27 18:04:10 +0000

    Changes from pulling down master on 27-01-2015

--- spec/factories/student_exam_tracks.rb
+++ spec/factories/student_exam_tracks.rb
@@ -13,6 +13,8 @@
 #  session_guid                    :string(255)
 #  course_module_id                :integer
 #  jumbo_quiz_taken                :boolean          default(FALSE)
+#  percentage_complete             :integer          default(0)
+#  count_of_cmes_completed         :integer          default(0)
 #
 
 FactoryGirl.define do
