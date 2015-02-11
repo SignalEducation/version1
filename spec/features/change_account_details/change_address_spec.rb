75c6d3a2584d44eee95e5e3276cf8d1423ec52d4
James Mac Redmond <james@learnsignal.com>
2015-02-11 10:57:58 +0000

    Added tests for change email and address

--- spec/features/change_account_details/change_address_spec.rb
+++ spec/features/change_account_details/change_address_spec.rb
@@ -1,32 +1,34 @@
 require 'rails_helper'
 require 'support/users_and_groups_setup'
 
-describe 'User changing their name' do
+describe 'User changing their email' do
 
   include_context 'users_and_groups_setup'
 
-  let!(:country_1) { FactoryGirl.create(:ireland)}
-  let!(:country_2) { FactoryGirl.create(:uk)}
+  let!(:country_1) { FactoryGirl.create(:ireland) }
+  let!(:country_2) { FactoryGirl.create(:uk) }
+  let!(:static_page) { FactoryGirl.create(:static_page) }
 
   before(:each) do
     activate_authlogic
   end
 
-  scenario 'when logged in as an individual_student_user' do
-    visit sign_in_path
+  scenario 'when logged in as an individual_student_user', js: true do
+    visit root_path
+    sleep 10
     within('.well.well-sm') do
       fill_in I18n.t('views.user_sessions.form.email'), with: individual_student_user.email
       fill_in I18n.t('views.user_sessions.form.password'), with: individual_student_user.password
       click_button I18n.t('views.general.go')
     end
     expect(page).to have_content 'Welcome back!'
+
     click_link('navbar-cog')
     click_link(I18n.t('views.users.show.h1'))
     expect(page).to have_content I18n.t('views.users.show.h1')
     click_link I18n.t('views.general.edit')
     expect(page).to have_content I18n.t('views.users.edit.h1')
-    fill_in I18n.t('views.users.form.first_name'), with: 'Student'
-    fill_in I18n.t('views.users.form.last_name'), with: 'Individual'
+    fill_in I18n.t('views.users.form.address_placeholder'), with: '123 Fake Street'
     click_button(I18n.t('views.general.save'))
     expect(page).to have_content 'User details have been updated successfully'
     expect(page).to have_content I18n.t('views.users.show.h1')
--- spec/features/change_account_details/change_email_spec.rb
+++ spec/features/change_account_details/change_email_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 require 'support/users_and_groups_setup'
 
-describe 'User changing their password' do
+describe 'User changing their email' do
 
   include_context 'users_and_groups_setup'
+
+  let!(:country_1) { FactoryGirl.create(:ireland)}
+  let!(:country_2) { FactoryGirl.create(:uk)}
 
   before(:each) do
     activate_authlogic
@@ -17,17 +20,16 @@ describe 'User changing their password' do
       click_button I18n.t('views.general.go')
     end
     expect(page).to have_content 'Welcome back!'
+
     click_link('navbar-cog')
     click_link(I18n.t('views.users.show.h1'))
     expect(page).to have_content I18n.t('views.users.show.h1')
-    click_link I18n.t('views.users.show.change_your_password.link')
-    expect(page).to have_content I18n.t('views.users.show.change_your_password.h4')
-    fill_in I18n.t('views.users.form.current_password'), with: individual_student_user.password
-    sleep 1
-    fill_in I18n.t('views.users.form.password'), with: 'abcabc123'
-    fill_in I18n.t('views.users.form.password_confirmation'), with: 'abcabc123'
-    click_button I18n.t('views.general.save')
-    expect(page).to have_content 'Your password has been updated successfully'
+    click_link I18n.t('views.general.edit')
+    expect(page).to have_content I18n.t('views.users.edit.h1')
+    fill_in I18n.t('views.users.form.email'), with: 'user@example.com'
+    click_button(I18n.t('views.general.save'))
+    expect(page).to have_content 'User details have been updated successfully'
+    expect(page).to have_content I18n.t('views.users.show.h1')
   end
 
 end
