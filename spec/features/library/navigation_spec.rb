26730fc7de10ddb8baee1d9a7b543361b93cc6bd
James Mac Redmond <james@learnsignal.com>
2015-02-12 09:14:03 +0000

    re#214 Changes to library navigation test to work with support/course_content

--- spec/features/library/navigation_spec.rb
+++ spec/features/library/navigation_spec.rb
@@ -7,19 +7,19 @@ describe 'User navigating through the library:' do
 
   let!(:static_page) { FactoryGirl.create(:landing_page) }
   let!(:subject_area_1) { FactoryGirl.create(:active_subject_area) }
-  let!(:subject_area_2) { FactoryGirl.create(:active_subject_area) }
   let!(:institution_1) { FactoryGirl.create(:active_institution, subject_area_id: subject_area_1.id) }
-  let!(:institution_2) { FactoryGirl.create(:active_institution, subject_area_id: subject_area_2.id) }
   let!(:qualification_1) { FactoryGirl.create(:active_qualification, institution_id: institution_1.id) }
-  let!(:qualification_2) { FactoryGirl.create(:active_qualification, institution_id: institution_2.id) }
   let!(:exam_level_1) { FactoryGirl.create(:active_exam_level_with_exam_sections, qualification_id: qualification_1.id) }
-  let!(:exam_level_2) { FactoryGirl.create(:active_exam_level_without_exam_sections, qualification_id: qualification_2.id) }
   let!(:exam_section_1) { FactoryGirl.create(:active_exam_section, exam_level_id: exam_level_1.id) }
   let!(:course_module_1) { FactoryGirl.create(:active_course_module, qualification_id: qualification_1.id, exam_section_id: exam_section_1.id) }
-  let!(:course_module_2) { FactoryGirl.create(:active_course_module, qualification_id: qualification_2.id, exam_level_id: exam_level_2.id) }
   let!(:course_module_element_1) { FactoryGirl.create(:cme_video, course_module_id: course_module_1.id) }
-  let!(:course_module_element_2) { FactoryGirl.create(:cme_quiz, course_module_id: course_module_2.id) }
   let!(:course_module_element_video_1) { FactoryGirl.create(:course_module_element_video, course_module_element_id: course_module_element_1.id) }
+  let!(:subject_area_2) { FactoryGirl.create(:active_subject_area) }
+  let!(:institution_2) { FactoryGirl.create(:active_institution, subject_area_id: subject_area_2.id) }
+  let!(:qualification_2) { FactoryGirl.create(:active_qualification, institution_id: institution_2.id) }
+  let!(:exam_level_2) { FactoryGirl.create(:active_exam_level_without_exam_sections, qualification_id: qualification_2.id) }
+  let!(:course_module_2) { FactoryGirl.create(:active_course_module, qualification_id: qualification_2.id, exam_level_id: exam_level_2.id) }
+  let!(:course_module_element_2) { FactoryGirl.create(:cme_quiz, course_module_id: course_module_2.id) }
   let!(:course_module_element_quiz_1) { FactoryGirl.create(:course_module_element_quiz, course_module_element_id: course_module_element_2.id) }
 
 
@@ -30,7 +30,6 @@ describe 'User navigating through the library:' do
   scenario 'user signs in then navigates down hierarchy to first cme', js: true  do
     visit root_path
     click_link 'Library'
-    binding.pry
   end
 
 end
