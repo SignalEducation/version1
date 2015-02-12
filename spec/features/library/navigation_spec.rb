2b3297eef383d73cd792519d77cab173e6edd90d
James Mac Redmond <james@learnsignal.com>
2015-02-12 15:01:02 +0000

    Added more steps to the library/navigation test

--- spec/features/library/navigation_spec.rb
+++ spec/features/library/navigation_spec.rb
@@ -16,7 +16,22 @@ describe 'User navigating through the library:' do
   scenario 'user signs in then navigates down hierarchy to first cme', js: true  do
     visit root_path
     click_link 'Library'
+    expect(page).to have_content I18n.t('views.general.home')
+    expect(page).to have_content I18n.t('views.library.show.h1').upcase
+    expect(page).to_not have_content I18n.t('views.general.back')
+    expect(page).to have_content I18n.t('views.subject_areas.index.h1').upcase
+    expect(page).to have_content 'Lorem Ipsum'.upcase
+    expect(page).to have_content 'Institute 1'.upcase
+    expect(page).to have_content 'Qualification 1'
+    within('#subject-area-panel') do
+      find_link('Subject Area 2').click
+    end
+    expect(page).to have_content I18n.t('views.subject_areas.index.h1').upcase
+    expect(page).to have_content 'Institute 2'.upcase
+    expect(page).to have_content 'Lorem Ipsum'.upcase
+    expect(page).to have_content 'Qualification 3'
     binding.pry
+
   end
 
 end
