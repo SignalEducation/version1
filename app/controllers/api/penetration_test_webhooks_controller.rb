ae0bb2ae1143fe84ccee630059add0366f441a11
Dan Laffan <dan.laffan@mac.com>
2015-02-12 10:41:06 +0000

    re#148 added a webhook API for TinfoilSecurity to call at the start and end of their tests

---
+++ app/controllers/api/penetration_test_webhooks_controller.rb
@@ -0,0 +1,25 @@
+class Api::PenetrationTestWebhooksController < ApplicationController
+
+  before_action :ensure_staging_or_dev
+
+  def test_starting
+    # do nothing for now
+  end
+
+  def test_complete
+    tf_user = User.where(last_name: 'Tinfoil').first
+    session_guid = UserActivityLog.where(user_id: tf_user.id).order(:created_at).last.session_guid
+    PenetrationTestCleanupWorker.perform_async(tf_user.id, session_guid)
+  end
+
+  protected
+
+  def ensure_staging_or_dev
+    unless Rails.env.staging? || Rails.env.development?
+      Rails.logger.error 'ERROR: api/PenetrationTestWebhooksController activated outside of Staging'
+      render nothing: true
+      false
+    end
+  end
+
+end
