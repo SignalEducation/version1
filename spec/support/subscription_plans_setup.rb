57c0db9768dadbe3b4e3b5027f6920d3cd926cad
Dan Laffan <dan.laffan@mac.com>
2015-02-15 16:10:50 +0000

    re#148 refactored and extended the student-sign-ups spec - the unhappy path and netsted describes

--- spec/support/subscription_plans_setup.rb
+++ spec/support/subscription_plans_setup.rb
@@ -16,78 +16,87 @@ shared_context 'subscription_plans_setup' do
   let!(:stripe_helper) { StripeMock.create_test_helper }
 
   # subscription plans - Euro - Mth / Qtr / Year
-  let!(:subscription_plan_eur_m) { FactoryGirl.create(:student_subscription_plan_m, currency_id: eur.id) }
+  let!(:subscription_plan_eur_m) { FactoryGirl.create(:student_subscription_plan_m,
+                                                      currency_id: eur.id, price: 10) }
   let!(:stripe_subscription_plan_eur_m) { StripeMock.start; plan = stripe_helper.create_plan(
           id: subscription_plan_eur_m.id,
-          amount: (subscription_plan_eur_m.price * 100).to_i);
+          amount: (subscription_plan_eur_m.price * 100).to_i)
           subscription_plan_eur_m.update_attributes(stripe_guid: plan.id)
           subscription_plan_eur_m.reload;  StripeMock.stop
           subscription_plan_eur_m
   }
-  let!(:subscription_plan_eur_q) { FactoryGirl.create(:student_subscription_plan_q, currency_id: eur.id) }
+  let!(:subscription_plan_eur_q) { FactoryGirl.create(:student_subscription_plan_q,
+                                                      currency_id: eur.id, price: 30) }
   let!(:stripe_subscription_plan_eur_q) { StripeMock.start; plan = stripe_helper.create_plan(
           id: subscription_plan_eur_q.id,
-          amount: (subscription_plan_eur_q.price * 100).to_i);
+          amount: (subscription_plan_eur_q.price * 100).to_i)
           subscription_plan_eur_q.update_attributes(stripe_guid: plan.id)
           subscription_plan_eur_q.reload;  StripeMock.stop
           subscription_plan_eur_q
   }
-  let!(:subscription_plan_eur_y) { FactoryGirl.create(:student_subscription_plan_y, currency_id: eur.id) }
+  let!(:subscription_plan_eur_y) { FactoryGirl.create(:student_subscription_plan_y,
+                                                      currency_id: eur.id, price: 120) }
   let!(:stripe_subscription_plan_eur_y) { StripeMock.start; plan = stripe_helper.create_plan(
           id: subscription_plan_eur_y.id,
-          amount: (subscription_plan_eur_y.price * 100).to_i);
+          amount: (subscription_plan_eur_y.price * 100).to_i)
           subscription_plan_eur_y.update_attributes(stripe_guid: plan.id)
           subscription_plan_eur_y.reload;  StripeMock.stop
           subscription_plan_eur_y
   }
 
   # subscription plans - USD - Mth / Qtr / Year
-  let!(:subscription_plan_usd_m) { FactoryGirl.create(:student_subscription_plan_m, currency_id: usd.id) }
+  let!(:subscription_plan_usd_m) { FactoryGirl.create(:student_subscription_plan_m,
+                                                      currency_id: usd.id, price: 11.50) }
   let!(:stripe_subscription_plan_usd_m) { StripeMock.start; plan = stripe_helper.create_plan(
           id: subscription_plan_usd_m.id,
-          amount: (subscription_plan_usd_m.price * 100).to_i);
+          amount: (subscription_plan_usd_m.price * 100).to_i)
           subscription_plan_usd_m.update_attributes(stripe_guid: plan.id)
           subscription_plan_usd_m.reload;  StripeMock.stop
           subscription_plan_usd_m
   }
-  let!(:subscription_plan_usd_q) { FactoryGirl.create(:student_subscription_plan_q, currency_id: usd.id) }
+  let!(:subscription_plan_usd_q) { FactoryGirl.create(:student_subscription_plan_q,
+                                                      currency_id: usd.id, price: 35) }
   let!(:stripe_subscription_plan_usd_q) { StripeMock.start; plan = stripe_helper.create_plan(
           id: subscription_plan_usd_q.id,
-          amount: (subscription_plan_usd_q.price * 100).to_i);
+          amount: (subscription_plan_usd_q.price * 100).to_i)
           subscription_plan_usd_q.update_attributes(stripe_guid: plan.id)
           subscription_plan_usd_q.reload;  StripeMock.stop
           subscription_plan_usd_q
   }
-  let!(:subscription_plan_usd_y) { FactoryGirl.create(:student_subscription_plan_y, currency_id: usd.id) }
+  let!(:subscription_plan_usd_y) { FactoryGirl.create(:student_subscription_plan_y,
+                                                      currency_id: usd.id, price: 134.99) }
   let!(:stripe_subscription_plan_usd_y) { StripeMock.start; plan = stripe_helper.create_plan(
           id: subscription_plan_usd_y.id,
-          amount: (subscription_plan_usd_y.price * 100).to_i);
+          amount: (subscription_plan_usd_y.price * 100).to_i)
           subscription_plan_usd_y.update_attributes(stripe_guid: plan.id)
           subscription_plan_usd_y.reload;  StripeMock.stop
           subscription_plan_usd_y
   }
 
   # subscription plans - GBP - Mth / Qtr / Year
-  let!(:subscription_plan_gbp_m) { FactoryGirl.create(:student_subscription_plan_m, currency_id: gbp.id) }
+  let!(:subscription_plan_gbp_m) { FactoryGirl.create(:student_subscription_plan_m,
+                                                      currency_id: gbp.id, price: 7.50) }
   let!(:stripe_subscription_plan_gbp_m) { StripeMock.start; plan = stripe_helper.create_plan(
           id: subscription_plan_gbp_m.id,
-          amount: (subscription_plan_gbp_m.price * 100).to_i);
+          amount: (subscription_plan_gbp_m.price * 100).to_i)
           subscription_plan_gbp_m.update_attributes(stripe_guid: plan.id)
           subscription_plan_gbp_m.reload;  StripeMock.stop
           subscription_plan_gbp_m
   }
-  let!(:subscription_plan_gbp_q) { FactoryGirl.create(:student_subscription_plan_q, currency_id: gbp.id) }
+  let!(:subscription_plan_gbp_q) { FactoryGirl.create(:student_subscription_plan_q,
+                                                      currency_id: gbp.id, price: 22.50) }
   let!(:stripe_subscription_plan_gbp_q) { StripeMock.start; plan = stripe_helper.create_plan(
           id: subscription_plan_gbp_q.id,
-          amount: (subscription_plan_gbp_q.price * 100).to_i);
+          amount: (subscription_plan_gbp_q.price * 100).to_i)
           subscription_plan_gbp_q.update_attributes(stripe_guid: plan.id)
           subscription_plan_gbp_q.reload;  StripeMock.stop
           subscription_plan_gbp_q
   }
-  let!(:subscription_plan_gbp_y) { FactoryGirl.create(:student_subscription_plan_y, currency_id: gbp.id) }
+  let!(:subscription_plan_gbp_y) { FactoryGirl.create(:student_subscription_plan_y,
+                                                      currency_id: gbp.id, price: 87.99) }
   let!(:stripe_subscription_plan_gbp_y) { StripeMock.start; plan = stripe_helper.create_plan(
           id: subscription_plan_gbp_y.id,
-          amount: (subscription_plan_gbp_y.price * 100).to_i);
+          amount: (subscription_plan_gbp_y.price * 100).to_i)
           subscription_plan_gbp_y.update_attributes(stripe_guid: plan.id)
           subscription_plan_gbp_y.reload;  StripeMock.stop
           subscription_plan_gbp_y
