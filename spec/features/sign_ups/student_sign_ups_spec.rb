51e69e920185bfd50845f0b967f18f76f84ba5c3
Dan Laffan <dan.laffan@mac.com>
2015-02-15 16:12:51 +0000

    re#148 refactored and extended the student-sign-ups spec - the unhappy path and netsted describes

--- spec/features/sign_ups/student_sign_ups_spec.rb
+++ spec/features/sign_ups/student_sign_ups_spec.rb
@@ -14,45 +14,106 @@ describe 'The student sign-up process', type: :feature do
     expect(page).to have_content maybe_upcase I18n.t('views.student_sign_ups.new.h1')
   end
 
-  scenario 'sign-up with valid details - Ireland and Euro', js: true do
-    complete_user_details('Dan','Smith', nil, ireland)
-    expect(page).to have_content '€'
-    complete_credit_card('valid')
-    sleep 10
-  end
+  #### The Happy Path
+  describe 'sign-up with valid details:' do
+    describe 'Euro / Ireland /' do
+      scenario 'Monthly', js: true do
+        student_sign_up_as('Dan', 'Murphy', nil, 'valid', eur, ireland, 1, true)
+      end
 
-  #### Helpers
+      scenario 'Quarterly', js: true do
+        student_sign_up_as('Eileen', 'McGee', nil, 'valid', eur, ireland, 3, true)
+      end
 
-  def complete_credit_card(card_type='valid')
-    case card_type
-      when 'valid'
-        complete_card_details('4242424242424242','123','12',Time.now.year + 1)
-      when 'expired'
-        complete_card_details('4242424242424242','123','12',Time.now.year - 1)
-      when 'declined'
-        complete_card_details('4242424242424242','123','12',Time.now.year - 1)
-      when 'bad_number'
-        complete_card_details('4242424242424242','123','12',Time.now.year - 1)
-      else
-        complete_card_details('4242424242424242','123','12',Time.now.year)
+      scenario 'Yearly', js: true do
+        student_sign_up_as('Tadhg', 'Muircheartaigh', nil, 'valid', eur, ireland, 12, true)
+      end
+    end
+
+    describe 'GBP / UK /' do
+      scenario 'Monthly', js: true do
+        student_sign_up_as('Arthur', 'Gasquione', nil, 'valid', gbp, uk, 1, true)
+      end
+
+      scenario 'Quarterly', js: true do
+        student_sign_up_as('Spencer', 'Hesketh-Smyth', nil, 'valid', gbp, uk, 3, true)
+      end
+
+      scenario ' Yearly', js: true do
+        student_sign_up_as('St.John', 'Peutty', nil, 'valid', gbp, uk, 12, true)
+      end
+    end
+
+    describe 'USD / USA /' do
+      scenario 'Monthly', js: true do
+        student_sign_up_as('Dale', 'Rothschild', nil, 'valid', usd, usa, 1, true)
+      end
+
+      scenario 'Quarterly', js: true do
+        student_sign_up_as('Joel', 'Goldman', nil, 'valid', usd, usa, 3, true)
+      end
+
+      scenario 'Yearly', js: true do
+        student_sign_up_as('Sean', 'Mahony', nil, 'valid', usd, usa, 12, true)
+      end
     end
   end
 
-  def complete_card_details(card, cvv, exp_month, exp_year)
-    fill_in('card_number', with: card)
-    fill_in('cvv_number', with: cvv)
-    select exp_month, from: 'expiry_month'
-    select exp_year, from: 'expiry_year'
+  #### The un-happy path
+
+  describe 'sign-up with problems:' do
+    describe 'bad user details -' do
+      scenario 'bad first name', js: true do
+        student_sign_up_as('D', 'Smith', nil, 'valid', eur, ireland, 1, false)
+      end
+
+      scenario 'bad last name', js: true do
+        student_sign_up_as('Dan', 'S', nil, 'valid', eur, ireland, 1, false)
+      end
+
+      scenario 'bad email', js: true do
+        student_sign_up_as('Jo', 'Ng', 'a@bcd', 'valid', eur, ireland, 1, false)
+      end
+    end
+
+    describe 'bad credit card details -' do
+      scenario 'card expired', js: true do
+        student_sign_up_as('Sean', 'Mahony', nil, 'expired', usd, usa, 12, false)
+      end
+
+      scenario 'bad cvc', js: true do
+        student_sign_up_as('Sean', 'Mahony', nil, 'bad_cvc', usd, usa, 12, false)
+      end
+
+      scenario 'card declined', js: true do
+        student_sign_up_as('Sean', 'Mahony', nil, 'declined', usd, usa, 12, false)
+      end
+
+      scenario 'card number invalid', js: true do
+        student_sign_up_as('Sean', 'Mahony', nil, 'bad_number', usd, usa, 12, false)
+      end
+
+      scenario 'card processing error', js: true do
+        student_sign_up_as('Sean', 'Mahony', nil, 'processing_error', usd, usa, 12, false)
+      end
+    end
   end
 
-  def complete_user_details(first_name, last_name, email=nil, country)
-    fill_in('user_first_name', with: first_name)
-    fill_in('user_last_name', with: last_name)
-    fill_in('user_email', with: email || "#{first_name.downcase}-#{rand(999999)}@example.com")
-    select country.name, from: 'user_country_id'
-    temp_password = ApplicationController.generate_random_code(10)
-    fill_in('user_password', with: temp_password)
-    fill_in('user_password_confirmation', with: temp_password)
+
+  #### Helpers
+
+  def student_sign_up_as(user_first_name, user_second_name, user_email, card_type, currency, country, subscription_months, expect_sign_up)
+    enter_user_details(user_first_name, user_second_name, user_email, country)
+    expect(page).to have_content currency.leading_symbol
+    student_picks_a_subscription_pllan(currency, subscription_months)
+    enter_credit_card_details(card_type)
+    click_button I18n.t('views.student_sign_ups.form.submit')
+    sleep 1
+    if expect_sign_up
+      expect(page).to have_content I18n.t('controllers.student_sign_ups.create.flash.success')
+    else
+      expect(page).not_to have_content I18n.t('controllers.student_sign_ups.create.flash.success')
+    end
   end
 
 end
