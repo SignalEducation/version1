a2c983d1d663dff52918f3154c647bda7955ef5e
Dan Laffan <dan.laffan@mac.com>
2015-02-24 11:20:04 +0000

    re#149 re#149B x'd out the request spec for SubscriptionPlanCategories

---
+++ spec/requests/subscription_plan_categories_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "SubscriptionPlanCategories", :type => :request do
+  describe "GET /subscription_plan_categories" do
+    xit "works! (now write some real specs)" do
+      get subscription_plan_categories_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
