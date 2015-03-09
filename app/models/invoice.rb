17c01ceec0714a42e96f04d1edb7440114a0dce9
Dan Laffan <dan.laffan@mac.com>
2015-03-09 14:19:53 +0000

    re#261 added livemode validation to Invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -52,6 +52,7 @@ class Invoice < ActiveRecord::Base
                   :original_stripe_data
 
   # Constants
+  STRIPE_LIVE_MODE = [(ENV['learnsignal_v3_stripe_live_mode'] == 'live')]
 
   # relationships
   belongs_to :currency
@@ -77,6 +78,7 @@ class Invoice < ActiveRecord::Base
   validates :total, presence: true
   validates :vat_rate_id, allow_nil: true,
             numericality: {only_integer: true, greater_than: 0}
+  validates :livemode, inclusion: {in: STRIPE_LIVE_MODE}
 
   # callbacks
 
