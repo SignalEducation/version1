6789ab1be806b17b5104ea40c90bb748b869ba11
Dan Laffan <dan.laffan@mac.com>
2015-03-24 14:58:44 +0000

    re#156 fixed a file management problem

--- config/environments/intercom.rb
+++
@@ -1,6 +0,0 @@
-IntercomRails.config do |config|
-
-  config.app_id = ENV['INTERCOM_APP_ID'] || 'yqda78s4' # live code = 'a81yx184'
-  config.enabled_environments = %w(development production staging)
-
-end
