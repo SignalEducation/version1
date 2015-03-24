69e085776bfa76e59355da422e91b31c8a4d8afb
Dan Laffan <dan.laffan@mac.com>
2015-03-24 14:55:36 +0000

    re#156 updated the intercom Gem and tweaked the config file

--- config/environments/intercom.rb
+++ config/environments/intercom.rb
@@ -1,6 +1,6 @@
 IntercomRails.config do |config|
 
-  config.app_id = ENV['Intercom_App_ID'] || 'yqda78s4' # live code = 'a81yx184'
+  config.app_id = ENV['INTERCOM_APP_ID'] || 'yqda78s4' # live code = 'a81yx184'
   config.enabled_environments = %w(development production staging)
 
 end
