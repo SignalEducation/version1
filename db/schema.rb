8a0a7f1b2cb0be9dbf374d7df5d1d9e31e22172b
Dan Laffan <dan.laffan@mac.com>
2015-03-31 16:57:38 +0100

    re#273 added CME_flash_card_packs model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316145035) do
+ActiveRecord::Schema.define(version: 20150331153710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 20150316145035) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "continent"
+  end
+
+  create_table "course_module_element_flash_card_packs", force: true do |t|
+    t.integer  "course_module_element_id"
+    t.string   "background_color"
+    t.string   "foreground_color"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "course_module_element_quizzes", force: true do |t|
