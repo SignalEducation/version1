37fe613efc26f4b1b2befc960376fe62632ebd87
Dan Laffan <dan.laffan@mac.com>
2015-04-22 17:16:51 +0100

    re#375 reworked the archivable model to also set active to false if active exists

--- app/models/concerns/archivable.rb
+++ app/models/concerns/archivable.rb
@@ -20,10 +20,11 @@ module Archivable
 
   def destroy
     # Assumes the model has a "destroyed_at" attribute.
-    self.update_attributes(destroyed_at: Proc.new{Time.now}.call)
-    if self.try(:destroyable_children)
-      self.destroyable_children.map {|x| x.destroy}
-    end
+    self.respond_to?(:active) ?
+            self.update_attributes(destroyed_at: Proc.new { Time.now }.call, active: false) :
+            self.update_attributes(destroyed_at: Proc.new { Time.now }.call)
+
+    self.destroyable_children.map { |x| x.destroy } if self.respond_to?(:destroyable_children)
   end
 
   def un_delete
