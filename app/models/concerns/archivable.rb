084bdf57e2ade9bc7927baa63e8c861a9be9695b
Dan Laffan <dan.laffan@mac.com>
2015-04-22 16:06:17 +0100

    re#375 updated the archivable module

--- app/models/concerns/archivable.rb
+++ app/models/concerns/archivable.rb
@@ -1,8 +1,21 @@
+# activate this module in any AR model using
+# include Archivable
+#
+# Add the following into the model:
+# def destroyable_children
+#   self.related_things.to_a + self.other_things.to_a
+# end
+#
+# Also, you should ensure that the model has a datetime called "destroyed_at"
+
 module Archivable
 
   def destroy
     # Assumes the model has a "destroyed_at" attribute.
     self.update_attributes(destroyed_at: Proc.new{Time.now}.call)
+    if self.try(:destroyable_children)
+      self.destroyable_children.map {|x| x.destroy}
+    end
   end
 
 end
