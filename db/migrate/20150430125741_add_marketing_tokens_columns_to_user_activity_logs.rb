adeb9f882bf2bc04bc091dbb9e7b0544f26d11e3
Dan Laffan <dan.laffan@mac.com>
2015-05-01 14:16:07 +0100

    re#370 added index:true to migration

--- db/migrate/20150430125741_add_marketing_tokens_columns_to_user_activity_logs.rb
+++ db/migrate/20150430125741_add_marketing_tokens_columns_to_user_activity_logs.rb
@@ -1,6 +1,6 @@
 class AddMarketingTokensColumnsToUserActivityLogs < ActiveRecord::Migration
   def change
-    add_column :user_activity_logs, :marketing_token_id, :integer
+    add_column :user_activity_logs, :marketing_token_id, :integer, index: true
     add_column :user_activity_logs, :marketing_token_cookie_issued_at, :datetime
   end
 end
