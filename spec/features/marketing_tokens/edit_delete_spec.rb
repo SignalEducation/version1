9588a8f0722835a333493aba889a99bbe8a5a6dd
Bosko Ivanisevic <bosko@toptal.com>
2015-05-05 13:49:29 +0200

    re#370 marketing tokens import, edit and delete feature tests

---
+++ spec/features/marketing_tokens/edit_delete_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+require 'support/users_and_groups_setup'
+
+describe 'Delete/Edit marketing tokens', type: :feature do
+
+  include_context 'users_and_groups_setup'
+
+  let!(:marketing_category) { FactoryGirl.create(:marketing_category, name: "Social Networks") }
+
+  before(:each) do
+    activate_authlogic
+  end
+
+  scenario "edit token as admin" do
+    token =  FactoryGirl.create(:marketing_token, marketing_category_id: marketing_category.id, is_hard: false)
+
+    sign_in_via_sign_in_page(admin_user)
+    visit marketing_tokens_path
+
+    within("tr##{token.id}") do
+      click_link(I18n.t('views.general.edit'))
+    end
+
+    check("marketing_token_is_hard")
+    click_button(I18n.t('views.general.save'))
+
+    within("tr##{token.id}") do
+      expect(page).to have_css("span.glyphicon-ok")
+    end
+  end
+
+  scenario "delete token as admin" do
+    token =  FactoryGirl.create(:marketing_token, marketing_category_id: marketing_category.id, is_hard: false)
+
+    sign_in_via_sign_in_page(admin_user)
+    visit marketing_tokens_path
+
+    within("//tr[@id='#{token.id}']") do
+      click_link(I18n.t('views.general.delete'))
+    end
+
+    expect(page).to have_no_content(token.code)
+  end
+end
