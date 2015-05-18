03fd0bd09bdd3227dec53e86bdb859cd6f6d1b30
James Mac Redmond <james@learnsignal.com>
2015-05-18 16:05:43 +0100

    Changed the config file for capistrano deploying to staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 #server 'stagingtemplate.learnsignal.com', user: 'ec2-user', roles: %w{app web},
-server 'staging3.learnsignal.com', user: 'ec2-user', roles: %w{app web},
+server 'staging9.learnsignal.com', user: 'ec2-user', roles: %w{app web},
        ssh_options: {keys: %w(~/.ssh/staging3-eu-key.pem),
                      auth_methods: %w(publickey)}
 
