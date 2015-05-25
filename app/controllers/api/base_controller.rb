2d05a7cb0ea6885551e9c50b2fdedcb04d5bd0df
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2015-05-25 13:38:01 +0200

    Store referral data in the cookie

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -3,6 +3,7 @@ class Api::BaseController < ApplicationController
   # application_controller stuff that we don't want here...
   skip_before_action :set_locale
   skip_before_action :set_session_stuff
+  skip_before_action :process_referral_code
   skip_before_action :process_marketing_tokens
   skip_before_action :log_user_activity
 
