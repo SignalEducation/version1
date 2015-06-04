335c65eb32b5ce15da8b7d00170b340375b12324
James Mac Redmond <james@learnsignal.com>
2015-06-04 10:22:15 +0100

    Fixed bug in content_denied partial with question_banks

--- db/schema.rb
+++ db/schema.rb
@@ -109,9 +109,9 @@ ActiveRecord::Schema.define(version: 20150602115257) do
     t.datetime "updated_at"
     t.integer  "course_module_jumbo_quiz_id"
     t.boolean  "is_jumbo_quiz",               default: false, null: false
+    t.integer  "seconds_watched",             default: 0
     t.boolean  "is_question_bank",            default: false, null: false
     t.integer  "question_bank_id"
-    t.integer  "seconds_watched",             default: 0
   end
 
   add_index "course_module_element_user_logs", ["corporate_customer_id"], name: "cme_user_logs_corporate_customer_id", using: :btree
