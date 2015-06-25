399c7dfc1a6f0f46389bdd7818d553d6dbf7a02e
James Mac Redmond <james@learnsignal.com>
2015-06-25 16:57:12 +0100

    Added to the HomePages migration file to auto create three home pages

--- db/migrate/20150608121356_create_home_pages.rb
+++ db/migrate/20150608121356_create_home_pages.rb
@@ -1,5 +1,5 @@
 class CreateHomePages < ActiveRecord::Migration
-  def change
+  def up
     create_table :home_pages do |t|
       t.string :seo_title
       t.string :seo_description
@@ -8,5 +8,23 @@ class CreateHomePages < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+
+    unless Rails.env.test?
+      HomePage.create(public_url: '/', seo_title: 'Learn Signal', seo_description: 'Learn Signal').tap do |home|
+        home.save
+        end
+      HomePage.create(public_url: '/acca', seo_title: 'Learn Signal', seo_description: 'Learn Signal').tap do |acca|
+        acca.save
+      end
+      HomePage.create(public_url: '/cfa', seo_title: 'Learn Signal', seo_description: 'Learn Signal').tap do |cfa|
+        cfa.save
+      end
+
+    end
+
+  end
+
+  def down
+    drop_table :home_pages
   end
 end
