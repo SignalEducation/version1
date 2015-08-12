c1107da6396c187b4f5f93873aeff67cb157ab3d
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2015-08-12 13:35:44 +0200

    Reformatted code

--- spec/factories/corporate_group_grants.rb
+++ spec/factories/corporate_group_grants.rb
@@ -1,10 +1,9 @@
 FactoryGirl.define do
   factory :corporate_group_grant do
-    corporate_group nil
-exam_level nil
-exam_section nil
-compulsory false
-restricted false
+    corporate_group_id 1
+    exam_level_id 1
+    exam_section_id nil
+    compulsory false
+    restricted false
   end
-
 end
