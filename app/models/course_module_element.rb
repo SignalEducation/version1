5a626c830739e11e074c4e5b23fed816a3a94f3a
James Mac Redmond <james@learnsignal.com>
2015-08-20 15:05:27 +0100

    Removed update_student_exam_tracks from StudentExamTrackWorker

--- app/models/course_module_element.rb
+++ app/models/course_module_element.rb
@@ -164,8 +164,11 @@ class CourseModuleElement < ActiveRecord::Base
   end
 
   def update_student_exam_tracks
-    StudentExamTracksWorker.perform_async(self.course_module_id)
-    true
+    #StudentExamTracksWorker.perform_async(self.course_module_id)
+    #true
+    StudentExamTrack.where(course_module_id: self.course_module_id).each do |set|
+      set.recalculate_completeness
+    end
   end
 
   def update_the_module_total_time_and_question_count
