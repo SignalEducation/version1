4b4db0778ba8c925eaf30133c2fc5d7c81c98b91
James Mac Redmond <james@learnsignal.com>
2015-09-08 15:27:52 +0100

    Changed the corporate student dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -51,6 +51,13 @@ class DashboardController < ApplicationController
     end
 
     if @dashboard_type.include?('corporate_student')
+      if current_user && current_user.corporate_student?
+        @exam_sections = @exam_sections.where('id not in (?)', current_user.restricted_exam_section_ids) unless current_user.restricted_exam_section_ids.empty?
+        compulsory_levels = ExamLevel.all_active.all_live.all_without_exam_sections_enabled.where(id: current_user.compulsory_exam_level_ids).all_in_order
+        compulsory_sections = ExamSection.all_active.all_live.where(id: current_user.compulsory_exam_section_ids).all_in_order
+        @compulsory_courses = compulsory_levels + compulsory_sections
+      end
+
 
     end
 
