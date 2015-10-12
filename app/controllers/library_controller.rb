ef9f376d3e4868d6d4de2a340a5638be7d2b32bf
James Mac Redmond <james@learnsignal.com>
2015-10-12 13:42:34 +0100

    Added if statement to library#show incase @course is nil

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -19,25 +19,29 @@ class LibraryController < ApplicationController
 
   def show
     @course = SubjectCourse.where(name_url: params[:subject_course_name_url].to_s).first
-    @duration = @course.try(:total_video_duration) + @course.try(:estimated_time_in_seconds)
-    if @course.corporate_customer_id
-      if current_user.nil? || (@course.restricted && (current_user.corporate_customer_id == nil || current_user.corporate_customer_id != @course.corporate_customer_id))
+    if @course.nil?
+      redirect_to library_url
+    else
+      @duration = @course.try(:total_video_duration) + @course.try(:estimated_time_in_seconds)
+      if @course.corporate_customer_id
+        if current_user.nil? || (@course.restricted && (current_user.corporate_customer_id == nil || current_user.corporate_customer_id != @course.corporate_customer_id))
+          redirect_to library_url
+        end
+      end
+      users_sets = StudentExamTrack.for_user_or_session(current_user.try(:id), current_session_guid).with_active_cmes.all_incomplete.all_in_order
+      user_course_sets = users_sets.where(subject_course_id: @course.try(:id))
+      latest_set = user_course_sets.first
+      @latest_element_id = latest_set.try(:latest_course_module_element_id)
+      @next_element = CourseModuleElement.where(id: @latest_element_id).first.try(:next_element)
+      if @course.try(:live)
+        render 'live_course'
+      elsif @course.try(:live) == false
+        render 'preview_course'
+      else
         redirect_to library_url
       end
+      seo_title_maker(@course.try(:name), @course.try(:seo_description), @course.try(:seo_no_index))
     end
-    users_sets = StudentExamTrack.for_user_or_session(current_user.try(:id), current_session_guid).with_active_cmes.all_incomplete.all_in_order
-    user_course_sets = users_sets.where(subject_course_id: @course.try(:id))
-    latest_set = user_course_sets.first
-    @latest_element_id = latest_set.try(:latest_course_module_element_id)
-    @next_element = CourseModuleElement.where(id: @latest_element_id).first.try(:next_element)
-    if @course.try(:live)
-      render 'live_course'
-    elsif @course.try(:live) == false
-      render 'preview_course'
-    else
-      redirect_to library_url
-    end
-    seo_title_maker(@course.try(:name), @course.try(:seo_description), @course.try(:seo_no_index))
   end
 
   def subscribe
