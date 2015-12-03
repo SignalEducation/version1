ab754a6a2438bbd588ca556237d5ca155f66cdf6
James Mac Redmond <james@learnsignal.com>
2015-12-03 15:05:54 +0000

    Removed more references to old course hierarchy

--- spec/factories/course_module_elements.rb
+++ spec/factories/course_module_elements.rb
@@ -33,7 +33,6 @@ FactoryGirl.define do
     estimated_time_in_seconds   1
     course_module_id            1
     sorting_order               1
-    forum_topic_id              1
     tutor_id                    1
     related_quiz_id             nil
     related_video_id            1
