ac5fc911bf8a99c212ecafbd6c0897381e386e63
James Mac Redmond <james@learnsignal.com>
2016-04-18 16:36:00 +0100

    Added some extra subdomains to exclude from the corporate subdomains

--- lib/subdomain.rb
+++ lib/subdomain.rb
@@ -1,7 +1,7 @@
 class Subdomain
   def self.matches?(request)
     case request.subdomain
-      when 'www', '', nil, 'learnsignal', 'staging'
+      when 'www', '', nil, 'learnsignal', 'staging', 'acca', 'cfa', 'forum', 'jobs'
         false
       else
         true
