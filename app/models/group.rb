0076198f25e771034751233877fe97b8dfbb03e2
James Mac Redmond <james@learnsignal.com>
2016-05-03 13:09:57 +0100

    Removed the text in next/previous bttons on show_results page

--- app/models/group.rb
+++ app/models/group.rb
@@ -40,7 +40,7 @@ class Group < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true, length: {maximum: 255}
   validates :name_url, presence: true, uniqueness: true, length: {maximum: 255}
   validates :description, presence: true
-  #validates :image, presence: true
+  validates :image, presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   # callbacks
