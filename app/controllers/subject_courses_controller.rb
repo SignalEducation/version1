4004df3b93e260af3c6f3012249b31bf8d47ee1f
James Mac Redmond <james@learnsignal.com>
2016-05-06 16:01:25 +0100

    Fixed failing controller tests

--- app/controllers/subject_courses_controller.rb
+++ app/controllers/subject_courses_controller.rb
@@ -106,6 +106,13 @@ class SubjectCoursesController < ApplicationController
     @course_modules = @subject_course.children
   end
 
+  def reorder
+    array_of_ids = params[:array_of_ids]
+    array_of_ids.each_with_index do |the_id, counter|
+      SubjectCourse.find(the_id.to_i).update_attributes(sorting_order: (counter + 1))
+    end
+    render json: {}, status: 200
+  end
 
   def destroy
     if @subject_course.destroy
