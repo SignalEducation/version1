59ce0487b96e22bbf7e38a15f861c7589e00130a
James Mac Redmond <james@learnsignal.com>
2016-05-17 11:02:18 +0100

    Added a routes_controller_spec file

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -3,11 +3,10 @@ class RoutesController < ApplicationController
   def root
     if current_user
       if current_user.corporate_customer?
-        custom_root = corporate_customer_url(current_user.corporate_customer)
+        redirect_to corporate_customer_url(current_user.corporate_customer)
       else
-        custom_root = dashboard_url
+        redirect_to dashboard_url
       end
-      redirect_to custom_root
     else
       if !request.subdomain.empty?
         case request.subdomain
