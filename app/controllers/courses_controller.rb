467e4a591a734814dff9067f9a72d42ba45d3fe2
James Mac Redmond <james@learnsignal.com>
2016-05-26 10:51:30 +0100

    Removed autoplay from video JS and chenged video_watched_data json method to set time_taken for CMEULs

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -93,8 +93,10 @@ class CoursesController < ApplicationController
     respond_to do |format|
       format.json {
         video_cme_user_log = CourseModuleElementUserLog.find_by_id(params[:course][:videoLogId])
+        cme = video_cme_user_log.course_module_element
         if video_cme_user_log
           video_cme_user_log.element_completed = true
+          video_cme_user_log.time_taken_in_seconds = cme.try(:duration)
           video_cme_user_log.save
         end
         render json: {}, status: :ok
