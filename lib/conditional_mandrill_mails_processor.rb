35801650e3c71063ed94b82e192a324ba1ba92b3
James Mac Redmond <james@learnsignal.com>
2016-08-17 12:46:15 +0100

    Reinstated the send_free_trial_ended_email that is triggered by a crontab rake command

--- lib/conditional_mandrill_mails_processor.rb
+++ lib/conditional_mandrill_mails_processor.rb
@@ -89,19 +89,18 @@ class ConditionalMandrillMailsProcessor
   end
 
   def self.process_free_trial_ended_notifications
-    free_plan = SubscriptionPlan.where(price: 0.0).first
-    return if free_plan.nil?
+    individual_user_group_id = UserGroup.find 1
+    free_trial_users = User.where(subscription_id: nil,
+                                  user_group_id: individual_user_group_id)
 
-    free_trial_subscriptions = Subscription.where(subscription_plan_id: free_plan.id,
-                                                  current_status: 'active')
-    free_trial_subscriptions.each do |subs|
-      if subs.free_trial_expired? &&
-         subs.user.trial_ended_notification_sent_at.nil? &&
-         subs.user.active?
-        MandrillWorker.perform_async(subs.user_id,
+    free_trial_users.each do |user|
+      if user.free_trial_expired? &&
+         user.trial_ended_notification_sent_at.nil? &&
+         user.active?
+        MandrillWorker.perform_async(user.id,
                                      "send_free_trial_ended_email",
-                                     url_helpers.user_new_paid_subscription_url(user_id: subs.user_id))
-        subs.user.update_attribute(:trial_ended_notification_sent_at, Time.now)
+                                     url_helpers.user_new_subscription_url(user_id: user.id))
+        user.update_attribute(:trial_ended_notification_sent_at, Time.now)
       end
     end
   end
--- lib/mandrill_client.rb
+++ lib/mandrill_client.rb
@@ -59,6 +59,12 @@ class MandrillClient
     send_template('acca-welcome-email', msg)
   end
 
+  def send_free_trial_ended_email(account_activation_url)
+    msg = message_stub.merge({"subject" => "You're free trial with learn signal has just ended"})
+    msg["global_merge_vars"] << { "name" => "ACCOUNTACTIVATIONURL", "content" => account_activation_url }
+    send_template('free-trial-ended', msg)
+  end
+
 
 
 
@@ -72,13 +78,6 @@ class MandrillClient
     msg["global_merge_vars"] << { "name" => "USERTRIALLENGTH", "content" => trial_length }
     send_template('subscription-error', msg)
   end
-
-  def send_free_trial_ended_email(account_activation_url)
-    msg = message_stub.merge({"subject" => ":( You're free trial with learn signal has just ended"})
-    msg["global_merge_vars"] << { "name" => "ACCOUNTACTIVATIONURL", "content" => account_activation_url }
-    send_template('free-trial-ended', msg)
-  end
-
 
   def send_account_reactivated_email(account_settings_url)
     msg = message_stub.merge({"subject" => "Your Account is Reactivated"})
