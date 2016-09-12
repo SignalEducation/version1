89d6dd793b7f1423d777720264c16fffd2e0df0b
James Mac Redmond <james@learnsignal.com>
2016-09-12 15:52:43 +0100

    Moved stripe product create and update code from controller to model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,20 +41,6 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(allowed_params)
-    stripe_product = Stripe::Product.create(name: @product.name, shippable: false, active: @product.active)
-    @product.live_mode = stripe_product.livemode
-    @product.stripe_guid = stripe_product.id
-    #Move this to the model
-    stripe_sku = Stripe::SKU.create(
-                                currency: @product.currency.iso_code,
-                                price: (@product.price.to_f * 100).to_i,
-                                product: @product.stripe_guid,
-                                active: @product.active,
-                                inventory: {
-                                    type: 'infinite'
-                                }
-    )
-    @product.stripe_sku_guid = stripe_sku.id
     if @product.save
       flash[:success] = I18n.t('controllers.products.create.flash.success')
       redirect_to products_url
@@ -65,12 +51,6 @@ class ProductsController < ApplicationController
 
 
   def update
-    #Move this to the model
-    stripe_product = Stripe::Product.retrieve(id: @product.stripe_guid)
-    stripe_product.name = @product.name
-    stripe_product.active = @product.active
-    @product.live_mode = stripe_product.livemode
-
     if @product.update_attributes(allowed_params)
       flash[:success] = I18n.t('controllers.products.update.flash.success')
       redirect_to products_url
@@ -101,7 +81,7 @@ class ProductsController < ApplicationController
   end
 
   def allowed_params
-    params.require(:product).permit(:name, :active, :subject_course_id, :mock_exam_id, :price, :currency_id, :livemode, :stripe_sku_guid)
+    params.require(:product).permit(:name, :active, :subject_course_id, :mock_exam_id, :price, :currency_id, :livemode)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -22,7 +22,7 @@ class Product < ActiveRecord::Base
   include LearnSignalModelExtras
 
   # attr-accessible
-  attr_accessible :name, :subject_course_id, :mock_exam_id, :active, :currency_id, :price, :stripe_sku_guid
+  attr_accessible :name, :subject_course_id, :mock_exam_id, :active, :currency_id, :price, :stripe_sku_guid, :live_mode
 
   # Constants
 
@@ -36,12 +36,14 @@ class Product < ActiveRecord::Base
   validates :name, presence: true
   validates :subject_course_id, presence: true,
             numericality: {only_integer: true, greater_than: 0}
-  validates :stripe_guid, presence: true, uniqueness: true
+  validates :stripe_guid, presence: true, uniqueness: true, on: :update
   validates :currency_id, presence: true
   validates :price, presence: true
-  validates :stripe_sku_guid, presence: true, uniqueness: true
+  validates :stripe_sku_guid, presence: true, uniqueness: true, on: :update
 
   # callbacks
+  after_create :create_on_stripe
+  after_update :update_on_stripe
   before_destroy :check_dependencies
 
   # scopes
@@ -53,6 +55,35 @@ class Product < ActiveRecord::Base
 
   # instance methods
   def destroyable?
+    false
+  end
+
+  def create_on_stripe
+    stripe_product = Stripe::Product.create(name: self.name, shippable: false, active: self.active)
+    self.live_mode = stripe_product.livemode
+    self.stripe_guid = stripe_product.id
+
+    stripe_sku = Stripe::SKU.create(
+        currency: self.currency.iso_code,
+        price: (self.price.to_f * 100).to_i,
+        product: self.stripe_guid,
+        active: self.active,
+        inventory: {
+            type: 'infinite'
+        }
+    )
+    self.stripe_sku_guid = stripe_sku.id
+    self.save!
+  end
+
+  def update_on_stripe
+    stripe_product = Stripe::Product.retrieve(id: self.stripe_guid)
+    stripe_product.name = self.name
+    stripe_product.active = self.active
+    stripe_product.save
+
+  rescue => e
+    errors.add(:stripe, e.message)
     false
   end
 
