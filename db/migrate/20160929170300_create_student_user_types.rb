9ca3215806bf7a5ae35d467e9a29020bf0a140a1
James Mac Redmond <james@learnsignal.com>
2016-10-18 13:15:37 +0100

    Removed the data creation code from migrations and added it to a rake task

--- db/migrate/20160929170300_create_student_user_types.rb
+++ db/migrate/20160929170300_create_student_user_types.rb
@@ -9,21 +9,6 @@ class CreateStudentUserTypes < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    unless Rails.env.test?
-      StudentUserType.new(name: 'Subscription Students', description: 'User with a valid subscription.', subscription: true, product_order: false).tap do |user_type|
-        user_type.save!
-      end
-      StudentUserType.new(name: 'Product Students', description: 'Users who have purchased a course with a one off payment', subscription: false, product_order: true).tap do |user_type|
-        user_type.save!
-      end
-      StudentUserType.new(name: 'No Access Students', description: 'Users who have an expired free trial or created an account but didnt finish the purchase process.', subscription: false, product_order: false).tap do |user_type|
-        user_type.save!
-      end
-      StudentUserType.new(name: 'Subscription and Product Students', description: 'Users who have both a valid subscription and have purchased at least one valid product course.', subscription: true, product_order: true).tap do |user_type|
-        user_type.save!
-      end
-    end
-
   end
 
   def down
--- db/migrate/20160929172555_add_student_user_type_id_to_user_model.rb
+++ db/migrate/20160929172555_add_student_user_type_id_to_user_model.rb
@@ -1,10 +1,6 @@
 class AddStudentUserTypeIdToUserModel < ActiveRecord::Migration
-  def up
+  def change
     add_column :users, :student_user_type_id, :integer, index: true
-  end
-
-  def down
-    remove_column :users, :student_user_type_id, :integer, index: true
   end
 
 end
