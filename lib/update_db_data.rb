593375499939e90429593c3a00421f92c7ffb301
James Mac Redmond <james@learnsignal.com>
2016-10-18 13:35:34 +0100

    Moved rake db code to a lib task that is called from the rake db

---
+++ lib/update_db_data.rb
@@ -0,0 +1,50 @@
+class UpdateDBData
+
+  def update_comp_user_group
+    user_group = UserGroup.find 9
+    user_group.update_attributes(complimentary: true, individual_student: false)
+  end
+
+  def create_student_user_types
+    StudentUserType.new(name: 'Free Trial Students', description: 'Users who are on a valid free trial.', subscription: false, product_order: false, free_trial: true).tap do |user_type|
+      user_type.save!
+    end
+    StudentUserType.new(name: 'Free Trial and Product Students', description: 'Users who are on a valid free trial. And have purchased a valid course product.', subscription: false, product_order: true, free_trial: true).tap do |user_type|
+      user_type.save!
+    end
+    StudentUserType.new(name: 'Subscription Students', description: 'User with a valid subscription.', subscription: true, product_order: false).tap do |user_type|
+      user_type.save!
+    end
+    StudentUserType.new(name: 'Product Students', description: 'Users who have purchased a course with a one off payment', subscription: false, product_order: true).tap do |user_type|
+      user_type.save!
+    end
+    StudentUserType.new(name: 'No Access Students', description: 'Users who have an expired free trial or created an account but didnt finish the purchase process.', subscription: false, product_order: false).tap do |user_type|
+      user_type.save!
+    end
+    StudentUserType.new(name: 'Subscription and Product Students', description: 'Users who have both a valid subscription and have purchased at least one valid product course.', subscription: true, product_order: true).tap do |user_type|
+      user_type.save!
+    end
+  end
+
+  def update_student_users
+    users = User.where(user_group_id: 1)
+    ActiveRecord::Base.transaction do
+      users.each do |user|
+
+        if user.free_trial && user.trial_limit_in_seconds <= ENV['free_trial_limit_in_seconds'].to_i
+          user.student_user_type_id = 5
+        elsif !user.free_trial && user.subscriptions.any? && user.active_subscription && ('active past_due canceled-pending').include?(user.active_subscription.current_status)
+          user.student_user_type_id = 1
+        elsif !user.free_trial && user.subscriptions.any? && user.active_subscription && ('active past_due canceled-pending').include?(user.active_subscription.current_status)
+          user.student_user_type_id = 3
+        else
+          user.student_user_type_id = 5
+        end
+        user.save!
+      end
+    end
+
+  end
+
+
+end
