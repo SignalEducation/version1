1ea133d5e104ed96975aa1686952a0b084f12aec
James Mac Redmond <james@learnsignal.com>
2016-10-18 11:29:48 +0100

    Added to migration file to update comp user group

--- db/migrate/20161009100249_add_complimentary_boolean_to_user_group_and_remove_old_product_fields.rb
+++ db/migrate/20161009100249_add_complimentary_boolean_to_user_group_and_remove_old_product_fields.rb
@@ -1,7 +1,19 @@
 class AddComplimentaryBooleanToUserGroupAndRemoveOldProductFields < ActiveRecord::Migration
-  def change
+  def up
     add_column :user_groups, :complimentary, :boolean, index: true, default: false
     remove_column :user_groups, :product_student, :boolean
     remove_column :user_groups, :product_required_to_see_content, :boolean
+
+    unless Rails.env.test? || Rails.env.development?
+      user_group = UserGroup.find 9
+      user_group.update_attributes(complimentary: true, individual_student: false)
+    end
+
+  end
+
+  def down
+    remove_column :user_groups, :complimentary, :boolean, index: true, default: false
+    add_column :user_groups, :product_student, :boolean
+    add_column :user_groups, :product_required_to_see_content, :boolean
   end
 end
