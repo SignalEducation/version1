5e0a017bb606dec177f11de30fbe64a9a1107245
James Mac Redmond <james@learnsignal.com>
2017-01-10 19:01:02 +0000

    Added small script to latest enrollments migration file to populate current migrations student_number with their users student_number

--- db/migrate/20170110171932_add_fields_to_enrollment.rb
+++ db/migrate/20170110171932_add_fields_to_enrollment.rb
@@ -6,5 +6,14 @@ class AddFieldsToEnrollment < ActiveRecord::Migration
     add_column :enrollments, :registered, :boolean, default: :false
   end
 
+  unless Rails.env.test?
+    users = User.where.not(student_number: nil).where.not(student_number: '')
+    users.each do |user|
+      user.enrollments.each do |enrollment|
+        enrollment.student_number = user.student_number
+        enrollment.save
+      end
+    end
+  end
 
 end
