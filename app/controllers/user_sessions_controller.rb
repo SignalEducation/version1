214d2ac0f14314df50a738aa336e6726f128c36d
James Mac Redmond <james@learnsignal.com>
2017-01-19 17:43:59 +0000

    Added another condition to before action in user_sessions

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,7 +3,6 @@ class UserSessionsController < ApplicationController
   before_filter :logged_out_required, only: [:new, :create]
   before_filter :logged_in_required,  only: :destroy
   before_filter :set_variables
-  before_filter :check_email_verification, only: [:create]
 
   def new
     @navbar = nil
@@ -46,7 +45,7 @@ class UserSessionsController < ApplicationController
 
   def check_email_verification
     user = User.find_by_email(params[:user_session][:email])
-    if !user.email_verified && user.individual_student?
+    if user && !user.email_verified && user.individual_student?
       flash[:warning] = 'The email for that account has not been verified. Please follow the instructions in the verification email we sent you.'
       redirect_to sign_in_url
     end
