c2b837f8375a13d479bd5b5073c1145bb69d05d0
James Mac Redmond <james@learnsignal.com>
2017-01-19 17:08:58 +0000

    Fixed issue loading group on account_verified page

--- app/controllers/student_sign_ups_controller.rb
+++ app/controllers/student_sign_ups_controller.rb
@@ -15,7 +15,7 @@ class StudentSignUpsController < ApplicationController
     #This is the post email verification page
     if current_user.topic_interest.present?
       home_page = HomePage.where(public_url: current_user.topic_interest).first
-      @group = home_page.group
+      @group = home_page.group if home_page
     end
     @groups = Group.for_public.all_active.all_in_order
   end
