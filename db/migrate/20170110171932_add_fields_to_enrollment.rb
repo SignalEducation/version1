8fd421a2a4a1dc0f8deed093ad09d5616179cfc2
James Mac Redmond <james@learnsignal.com>
2017-01-20 10:15:21 +0000

    Removed loop from db file

--- db/migrate/20170110171932_add_fields_to_enrollment.rb
+++ db/migrate/20170110171932_add_fields_to_enrollment.rb
@@ -6,13 +6,4 @@ class AddFieldsToEnrollment < ActiveRecord::Migration
     add_column :enrollments, :registered, :boolean, default: :false
   end
 
-  unless Rails.env.test?
-    users = User.where.not(student_number: nil).where.not(student_number: '')
-    users.each do |user|
-      user.enrollments.each do |enrollment|
-        enrollment.update_attribute(:student_number, user.student_number) if user.student_number
-      end
-    end
-  end
-
 end
