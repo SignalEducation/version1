89ef57f5c862048014d73111647f04c1e69ccb45
James Mac Redmond <james@learnsignal.com>
2017-01-27 10:10:46 +0000

    Made changes to enrollment worker for study streak email url generation

--- app/workers/enrollment_email_worker.rb
+++ app/workers/enrollment_email_worker.rb
@@ -1,5 +1,6 @@
 class EnrollmentEmailWorker
   include Sidekiq::Worker
+  delegate :url_helpers, to: 'Rails.application.routes'
   require 'mandrill_client'
 
   sidekiq_options queue: 'medium'
@@ -10,7 +11,16 @@ class EnrollmentEmailWorker
     @enrollment = @subject_course_user_log.enrollment
     @corporate = nil
     @course_name = @subject_course_user_log.subject_course.name
-    @url = Rails.application.routes.url_helpers.course_url(host: Rails.application.routes.default_url_options[:host], subject_course_name_url: @subject_course_user_log.subject_course.name_url, course_module_name_url: @subject_course_user_log.last_element.next_element.course_module.name_url, course_module_element_name_url: @subject_course_user_log.last_element.next_element.name_url)
+    if @subject_course_user_log.last_element.next_element.class == CourseModuleElement
+      @url = url_helpers.course_path(subject_course_name_url: @subject_course_user_log.subject_course.name_url, course_module_name_url: @subject_course_user_log.last_element.course_module.name_url, course_module_element_name_url: @subject_course_user_log.last_element.next_element.name_url, host: Rails.env.test? ? "www.example.com" : Rails.application.routes.default_url_options[:host])
+    else
+      if @subject_course_user_log.subject_course.subject_course_category == SubjectCourseCategory.default_subscription_category
+        @url = url_helpers.subscription_course_url(subject_course_name_url: @subject_course_user_log.subject_course.name_url, host: Rails.env.test? ? "www.example.com" : Rails.application.routes.default_url_options[:host])
+      else
+        @url = url_helpers.diploma_course_url(subject_course_name_url: @subject_course_user_log.subject_course.name_url, host: Rails.env.test? ? "www.example.com" : Rails.application.routes.default_url_options[:host])
+      end
+    end
+
 
     template_args = [@url, @course_name]
     if @user && @user.email && @user.individual_student?
