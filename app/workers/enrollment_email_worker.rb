72d14dcc25399a738f86dc5f4af4995a260f2ebc
James Mac Redmond <james@learnsignal.com>
2017-02-27 12:43:02 +0000

    Tidid up the enrollments email worker

--- app/workers/enrollment_email_worker.rb
+++ app/workers/enrollment_email_worker.rb
@@ -7,16 +7,17 @@ class EnrollmentEmailWorker
   def perform(email, scul_id, datetime_triggered, method_name)
     @user = User.where(email: email).first
     @subject_course_user_log = SubjectCourseUserLog.find(scul_id)
+    @course = @subject_course_user_log.subject_course
     @enrollment = @subject_course_user_log.enrollment
-    @course_name = @subject_course_user_log.subject_course.name
+
     if @subject_course_user_log.last_element.next_element.class == CourseModuleElement
-      @url = Rails.application.routes.url_helpers.course_url(subject_course_name_url: @subject_course_user_log.subject_course.name_url, course_module_name_url: @subject_course_user_log.last_element.course_module.name_url, course_module_element_name_url: @subject_course_user_log.last_element.next_element.name_url, host: ENV['learnsignal_v3_server_email_domain'])
+      @url = Rails.application.routes.url_helpers.course_url(subject_course_name_url: @course.name_url, course_module_name_url: @subject_course_user_log.last_element.course_module.name_url, course_module_element_name_url: @subject_course_user_log.last_element.next_element.name_url, host: ENV['learnsignal_v3_server_email_domain'])
     else
-      @url = Rails.application.routes.url_helpers.subscription_course_url(subject_course_name_url: @subject_course_user_log.subject_course.name_url, host: Rails.env.test? ? "www.example.com" : Rails.application.routes.default_url_options[:host])
+      @url = Rails.application.routes.url_helpers.library_course_url(group_name_url: @course.parent.name_url, subject_course_name_url: @course.name_url, host: ENV['learnsignal_v3_server_email_domain'])
     end
 
 
-    template_args = [@url, @course_name]
+    template_args = [@url, @course.name]
     if @user && @user.email && @user.individual_student?
       if !@subject_course_user_log.completed && (@enrollment.updated_at.to_i < datetime_triggered + 2.hours.to_i)
         mc = MandrillClient.new(@user)
