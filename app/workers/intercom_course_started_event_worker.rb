9975d9c7327594761916de36d944335e22cc7305
James Mac Redmond <james@learnsignal.com>
2017-03-14 21:55:33 +0000

    Add a if to all intercom workers to ensure they only execute if they find a valid user

--- app/workers/intercom_course_started_event_worker.rb
+++ app/workers/intercom_course_started_event_worker.rb
@@ -5,20 +5,22 @@ class IntercomCourseStartedEventWorker
 
   def perform(user_id, course_name)
     user = User.where(id: user_id).first
-    intercom = Intercom::Client.new(
-        app_id: ENV['intercom_app_id'],
-        api_key: ENV['intercom_api_key']
-    )
+    if user
+      intercom = Intercom::Client.new(
+          app_id: ENV['intercom_app_id'],
+          api_key: ENV['intercom_api_key']
+      )
 
-    intercom.events.create(
-        :event_name => "Started Course",
-        :created_at => Time.now.to_i,
-        :user_id => user_id,
-        :email => user.email,
-        :metadata => {
-            "course_name" => course_name
-        }
-    )
+      intercom.events.create(
+          :event_name => "Started Course",
+          :created_at => Time.now.to_i,
+          :user_id => user_id,
+          :email => user.email,
+          :metadata => {
+              "course_name" => course_name
+          }
+      )
+    end
   end
 
 end
--- app/workers/intercom_create_user_worker.rb
+++ app/workers/intercom_create_user_worker.rb
@@ -5,25 +5,27 @@ class IntercomCreateUserWorker
 
   def perform(user_id)
     user = User.where(id: user_id).first
-    intercom = Intercom::Client.new(
-        app_id: ENV['intercom_app_id'],
-        api_key: ENV['intercom_api_key']
-    )
+    if user
+      intercom = Intercom::Client.new(
+          app_id: ENV['intercom_app_id'],
+          api_key: ENV['intercom_api_key']
+      )
 
-    intercom.users.create(user_id: user_id,
-                          email: user.email,
-                          name: user.full_name,
-                          created_at: user.created_at,
-                          custom_data: {guid: user.guid,
-                                        user_group: user.user_group,
-                                        account_status: user.user_account_status,
-                                        free_trial: user.free_member?,
-                                        valid_free_trial: user.valid_free_member?,
-                                        free_trial_days_left: user.days_left,
-                                        free_trial_minutes_left: user.minutes_left,
-                                        email_verified: user.email_verified,
-                                        topic_interest: user.topic_interest,
-                          })
+      intercom.users.create(user_id: user_id,
+                            email: user.email,
+                            name: user.full_name,
+                            created_at: user.created_at,
+                            custom_data: {guid: user.guid,
+                                          user_group: user.user_group,
+                                          account_status: user.user_account_status,
+                                          free_trial: user.free_member?,
+                                          valid_free_trial: user.valid_free_member?,
+                                          free_trial_days_left: user.days_left,
+                                          free_trial_minutes_left: user.minutes_left,
+                                          email_verified: user.email_verified,
+                                          topic_interest: user.topic_interest,
+                            })
+    end
   end
 
 end
