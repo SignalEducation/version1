03eea2e5ec51ff4526549102dfab9b7e861776b9
James Mac Redmond <james@learnsignal.com>
2017-03-28 12:31:45 +0100

    Reinstated the set password process for admin invite user creation, also add the Upgrade Account link back to navbars for expired free_trial users

--- app/controllers/user_password_resets_controller.rb
+++ app/controllers/user_password_resets_controller.rb
@@ -49,6 +49,47 @@ class UserPasswordResetsController < ApplicationController
     end
   end
 
+  #Admin invite allow user to input password
+  def set_password
+    if params[:id].to_s.length == 20
+      @user = User.where(password_reset_token: params[:id].to_s).first
+      if @user
+        render :set_password
+      else
+        flash[:error] = I18n.t('controllers.user_password_resets.edit.flash.error')
+        redirect_to root_url
+      end
+    else
+      flash[:error] = I18n.t('controllers.user_password_resets.edit.flash.error')
+      redirect_to root_url
+    end
+    seo_title_maker('Set password for your account', 'Please enter a valid password to finish setting up your account.', true)
+  end
+
+  #Admin invite update user to set the password
+  def create_password
+    if params[:password] == params[:password_confirmation]
+      # in the params, params[:id] holds the reset_token.
+      @user = User.finish_password_reset_process(params[:id], params[:password], params[:password_confirmation])
+      if @user
+        @user_session = UserSession.create(@user)
+        flash[:success] = I18n.t('controllers.user_password_resets.update.flash.success')
+        @user.update_attributes(password_change_required: nil, session_key: session[:session_id])
+        redirect_back_or_default library_url
+      else
+        @user = User.find_by_password_reset_token(params[:id])
+        flash[:error] = I18n.t('controllers.user_password_resets.update.flash.error')
+        render :set_password
+      end
+    else
+      @user = User.find_by_password_reset_token(params[:id])
+      redirect_to root_url if @user.nil?
+      flash[:error] = I18n.t('controllers.user_password_resets.update.flash.password_and_confirmation_do_not_match')
+      render :edit
+    end
+  end
+
+
   protected
 
   def get_variables
