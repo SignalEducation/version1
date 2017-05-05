eac416f986cbc29b5d0fcda3b8322af2579b335d
James Mac Redmond <james@learnsignal.com>
2017-05-05 12:00:11 +0100

    Added new route to catch old library url, stopping it from being catched by the home_pages route

--- config/routes.rb
+++ config/routes.rb
@@ -127,6 +127,7 @@ Rails.application.routes.draw do
 
     get 'library', to: 'library#index', as: :library
     get 'library/:group_name_url', to: 'library#group_show', as: :library_group
+    get 'group/:group_name_url', to: 'library#group_show'
     get 'library/:group_name_url/:subject_course_name_url', to: 'library#course_show', as: :library_course
     get 'library/:group_name_url/:subject_course_name_url/preview', to: 'library#preview_course', as: :preview_course
 
