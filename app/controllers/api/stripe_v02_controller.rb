4c7efcbae0a4c38c5cbd03c2d55111777152f1c9
James Mac Redmond <james@learnsignal.com>
2017-06-21 12:06:31 +0100

    Added a rails logger to stripe v02 controller

--- app/controllers/api/stripe_v02_controller.rb
+++ app/controllers/api/stripe_v02_controller.rb
@@ -5,14 +5,18 @@ class Api::StripeV02Controller < Api::BaseController
   def create
     raw_json = request.body.read
     event_json = JSON.parse(raw_json) unless raw_json.blank?
+
+    Rails.logger.error "INFO: Api/StripeV02#Create: Notice: event-type: #{event_json["type"]}, api-version: #{event_json["api_version"]}"
+
     if event_json && StripeApiEvent::KNOWN_PAYLOAD_TYPES.include?(event_json["type"]) && StripeApiEvent::KNOWN_API_VERSIONS.include?(event_json["api_version"])
 
       StripeApiProcessorWorker.perform_async(event_json["id"],
                                              event_json["api_version"],
                                              account_url)
 
+      render text: nil, status: 204
     end
-    render text: nil, status: 204
+    render text: nil, status: 404 # Let Stripe try again to send data
   rescue => e
     Rails.logger.error "ERROR: Api/StripeV02#Create: Error: #{e.inspect}\nRaw event: #{raw_json}"
     render text: nil, status: 404 # Let Stripe try again to send data
