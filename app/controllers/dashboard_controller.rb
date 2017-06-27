dc4f92b4e40f09c876f64edd187db86a2b7a3366
James Mac Redmond <james@learnsignal.com>
2017-06-27 11:42:22 +0100

    Added a new CFA tab to the dashboard to display all CFA courses if the user has done any cfa content

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -109,6 +109,10 @@ class DashboardController < ApplicationController
     log_ids = @logs.map(&:id)
     @non_enrollment_log_ids = log_ids - enrollment_ids
     @non_enrollment_logs = SubjectCourseUserLog.where(id: @non_enrollment_log_ids)
+    @cfa_group = Group.where(name_url: 'cfa-revision').first
+    @cfa_courses = @cfa_group.subject_courses
+    cfa_course_ids = @cfa_courses.map(&:id)
+    @cfa_logs = @logs.where(subject_course_id: cfa_course_ids)
   end
 
   def tutor
