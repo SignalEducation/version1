16765618ca0171c2a99f85ba406e43d9564e0831
James Mac Redmond <james@learnsignal.com>
2017-08-30 14:02:26 +0100

    added new checkbow design to both sign up forms

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -42,6 +42,7 @@ class SubscriptionsController < ApplicationController
   end
 
   def create_subscription
+    #TODO Reconfigure to use allowed_params
     user = User.find(params[:user_id])
     subscription_params = params[:user][:subscriptions_attributes]["0"]
     subscription_plan = SubscriptionPlan.find(subscription_params["subscription_plan_id"].to_i)
