418d88c2dac06d4b114fb4ab726059d417bcbd0c
James Mac Redmond <james@learnsignal.com>
2018-01-31 10:30:24 +0000

    Fixed the sitemap generator file

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,56 +5,37 @@ host 'learnsignal.com'
 sitemap :site do
 
   #Landing Pages
-  url root_url, last_mod: Time.now, change_freq: 'weekly', priority: 1.0
-  business_page = HomePage.where(public_url: '/business').first
-  acca_page = HomePage.where(public_url: '/acca').first
-  cfa_page = HomePage.where(public_url: '/cfa').first
-  wso_page = HomePage.where(public_url: '/wso').first
-  url business_url, last_mod: business_page.updated_at, change_freq: 'weekly', priority: 1.0
-  url acca_url, last_mod: acca_page.updated_at, change_freq: 'weekly', priority: 1.0
-  url cfa_url, last_mod: cfa_page.updated_at, change_freq: 'weekly', priority: 1.0
-  url wso_url, last_mod: wso_page.updated_at, change_freq: 'weekly', priority: 1.0
+  url root_url, last_mod: Time.now, change_freq: 'monthly', priority: 1.0
+
+  HomePage.where(home: false).each do |home_page|
+    url "#{host}/#{home_page.public_url}", last_mod: home_page.updated_at, change_freq: 'monthly', priority: 1.0
+  end
+
 
   #Pricing page
   last_updated_plan = SubscriptionPlan.generally_available.for_students.all_active.all_in_update_order.last
   url pricing_url, last_mod: last_updated_plan.updated_at, change_freq: 'monthly', priority: 1.0
+
   #Tutor Profile pages
   User.all_tutors.where.not(profile_image_file_name: nil).each do |tutor|
-    url profile_url(tutor, locale: 'en'), last_mod: tutor.updated_at, change_freq: 'weekly', priority: 1.0
+    url profile_url(tutor, locale: 'en'), last_mod: tutor.updated_at, change_freq: 'monthly', priority: 1.0
   end
+
   #Footer pages + other static pages
-  url why_learn_signal_url, last_mod: Time.now, change_freq: 'monthly', priority: 1.0
-  url careers_url, last_mod: Time.now, change_freq: 'monthly', priority: 1.0
   url tutors_url, last_mod: Time.now, change_freq: 'monthly', priority: 1.0
   url contact_url, last_mod: Time.now, change_freq: 'monthly', priority: 1.0
   url terms_and_conditions_url, last_mod: Time.now, change_freq: 'monthly', priority: 1.0
 
   #Library
-  def library_special_link(the_thing)
-    if the_thing.class == Group
-      the_thing = the_thing
-      library_group_url(
-          the_thing.name_url,
-          locale: 'en'
-      )
-    elsif the_thing.class == SubjectCourse
-      the_thing = the_thing
-      library_course_url(
-          the_thing.name_url,
-          locale: 'en'
-      )
-    else
-      library_url
-    end
+  group = Group.all_active.all_in_order.first
+  url library_url, last_mod: group.updated_at, change_freq: 'monthly', priority: 1.0
+
+  group.active_children.all_in_order.each do |course|
+    url library_course_url(group_name_url: group.name_url, subject_course_name_url: course.name_url), last_mod: course.updated_at, change_freq: 'monthly', priority: 1.0
   end
-  url library_url, last_mod: Time.now, change_freq: 'monthly', priority: 1.0
-  Group.all_active.each do |group|
-    url library_special_link(group), last_mod: group.updated_at, change_freq: 'weekly', priority: 1.0
-  end
-  SubjectCourse.all_active.all_not_restricted.each do |course|
-    url library_special_link(course), last_mod: course.updated_at, change_freq: 'weekly', priority: 1.0
-  end
+
   #Sign In, Forgot Password
+  url new_student_url, last_mod: Time.now, change_freq: 'monthly', priority: 1.0
   url sign_in_url, last_mod: Time.now, change_freq: 'monthly', priority: 1.0
   url forgot_password_url, last_mod: Time.now, change_freq: 'monthly', priority: 1.0
 
