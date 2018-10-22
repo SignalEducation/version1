50fd5e2cf3b584835bc58e62a2be9cc984154380
James Mac Redmond <james@learnsignal.com>
2018-10-22 11:38:48 +0100

    Initial update to api v02 controller tests

--- spec/controllers/api/stripe_v02_controller_spec.rb
+++ spec/controllers/api/stripe_v02_controller_spec.rb
@@ -1,73 +1,45 @@
 require 'rails_helper'
-require 'mandrill_client'
-require 'support/users_and_groups_setup'
-require 'support/system_setup'
-require 'stripe_mock'
+require 'support/stripe_web_mock_helpers'
+
 
 describe Api::StripeV02Controller, type: :controller do
 
-  include_context 'users_and_groups_setup'
-  include_context 'system_setup'
+  let!(:gbp) { FactoryBot.create(:gbp) }
+  let!(:uk) { FactoryBot.create(:uk, currency_id: gbp.id) }
+  let!(:uk_vat_code) { FactoryBot.create(:vat_code, country_id: uk.id) }
+  let!(:subscription_plan_gbp_m) { FactoryBot.create(:student_subscription_plan_m,
+                                                     currency_id: gbp.id, price: 7.50, stripe_guid: 'stripe_plan_guid_m') }
+  let!(:subscription_plan_gbp_q) { FactoryBot.create(:student_subscription_plan_q,
+                                                     currency_id: gbp.id, price: 22.50, stripe_guid: 'stripe_plan_guid_q') }
+  let!(:subscription_plan_gbp_y) { FactoryBot.create(:student_subscription_plan_y,
+                                                     currency_id: gbp.id, price: 87.99, stripe_guid: 'stripe_plan_guid_y') }
+  let!(:student_user_group ) { FactoryBot.create(:student_user_group ) }
+  let!(:valid_trial_student) { FactoryBot.create(:valid_free_trial_student,
+                                                 user_group_id: student_user_group.id) }
+  let!(:valid_trial_student_access) { FactoryBot.create(:valid_free_trial_student_access,
+                                                        user_id: valid_trial_student.id) }
+  let!(:valid_subscription_student) { FactoryBot.create(:valid_subscription_student,
+                                                        user_group_id: student_user_group.id) }
+  let!(:valid_subscription_student_access) { FactoryBot.create(:trial_student_access,
+                                                               user_id: valid_subscription_student.id) }
 
-  let!(:start_stripe_mock) { StripeMock.start }
-  let(:stripe_helper) { StripeMock.create_test_helper }
-  let!(:subscription_plan_m) { FactoryBot.create(:student_subscription_plan_m) }
-  let!(:subscription_plan_q) { FactoryBot.create(:student_subscription_plan_q) }
+  let!(:valid_subscription) { FactoryBot.create(:valid_subscription, user_id: valid_subscription_student.id,
+                                                subscription_plan_id: subscription_plan_gbp_m.id,
+                                                stripe_customer_id: valid_subscription_student.stripe_customer_id ) }
+  let!(:default_card) { FactoryBot.create(:subscription_payment_card, user_id: valid_subscription_student.id,
+                                          is_default_card: true, stripe_card_guid: 'guid_222',
+                                          status: 'card-live' ) }
 
-  let!(:student) { FactoryBot.create(:student_user) }
-  let!(:student_2) { FactoryBot.create(:student_user) }
-  let!(:reactivating_student) { FactoryBot.create(:student_user) }
-  let!(:canceled_pending_student) { FactoryBot.create(:student_user) }
-  let!(:card_details_1) { {number: '4242424242424242', cvc: '123', exp_month: '12', exp_year: '2019'} }
-  let!(:card_details_2) { {number: '4242424242424242', cvc: '123', exp_month: '11', exp_year: '2019'} }
-  let!(:card_token_1)   { Stripe::Token.create(card: card_details_1) }
-  let!(:card_token_2)   { Stripe::Token.create(card: card_details_2) }
-
-  let!(:subscription_1) { FactoryBot.create(:subscription, user_id: student.id, subscription_plan_id: subscription_plan_m.id, active: true) }
-  let!(:subscription_2) { FactoryBot.create(:subscription, user_id: student_2.id, subscription_plan_id: subscription_plan_m.id, stripe_token: card_token_1.id, active: true) }
-  let!(:subscription_3) { FactoryBot.create(:subscription, user_id: reactivating_student.id, subscription_plan_id: subscription_plan_m.id, current_status: 'active', active: true) }
-  let!(:subscription_4) { FactoryBot.create(:subscription, user_id: canceled_pending_student.id, subscription_plan_id: subscription_plan_m.id, current_status: 'canceled-pending', active: true) }
-
-  #Stripe Webhook objects
-  let!(:invoice_created_event_1) {
-    StripeMock.mock_webhook_event('invoice.created',
-                                  subscription: subscription_1.stripe_guid,
-                                  customer: student.reload.stripe_customer_id) }
-  let!(:invoice_created_event_2) {
-    StripeMock.mock_webhook_event('invoice.created',
-                                  subscription: subscription_2.stripe_guid,
-                                  customer: student.reload.stripe_customer_id) }
-  let!(:invoice_payment_failed_event_1) {
-    StripeMock.mock_webhook_event('invoice.payment_failed',
-                                  subscription: subscription_2.stripe_guid,
-                                  next_payment_attempt: (Time.new + 24.hours),
-                                  customer: student_2.reload.stripe_customer_id) }
-  let!(:invoice_payment_failed_event_2) {
-    StripeMock.mock_webhook_event('invoice.payment_failed',
-                                  subscription: subscription_2.stripe_guid,
-                                  customer: student_2.reload.stripe_customer_id) }
-  let!(:invoice_payment_succeeded_event_1) {
-    StripeMock.mock_webhook_event('invoice.payment_succeeded',
-                                  subscription: subscription_1.stripe_guid,
-                                  customer: student.reload.stripe_customer_id) }
-  let!(:invoice_payment_succeeded_event_2) {
-    StripeMock.mock_webhook_event('invoice.payment_succeeded',
-                                  subscription: subscription_3.stripe_guid,
-                                  customer: student.reload.stripe_customer_id) }
-  let!(:customer_subscription_deleted_event) {
-    StripeMock.mock_webhook_event('customer.subscription.deleted',
-                                  subscription: subscription_4.stripe_guid,
-                                  customer: canceled_pending_student.reload.stripe_customer_id) }
 
 
   describe "POST 'create'" do
     describe 'preliminary functionality: ' do
-      xit 'returns 204 when called with no payload' do
+      it 'returns 204 when called with no payload' do
         post :create
         expect(response.status).to eq(204)
       end
 
-      xit 'logs an error if invalid JSON is received' do
+      it 'logs an error if invalid JSON is received' do
         expect(Rails.logger).to receive(:error)
         post :create, event: {id: '123'}
         expect(response.status).to eq(404)
@@ -76,28 +48,28 @@ describe Api::StripeV02Controller, type: :controller do
 
     describe 'dealing with payload data:' do
       describe 'with valid data' do
-        before(:each) do
-          SubscriptionPlan.skip_callback(:update, :before, :update_on_stripe_platform)
-          subscription_plan_m.stripe_guid = invoice_created_event_1.data.object.lines.data[0].plan.id
-          subscription_plan_m.save
-          subscription_1.update_attribute(:stripe_guid, invoice_created_event_1.data.object.subscription)
-        end
 
-        xit 'invoice.created event' do
-          post :create, invoice_created_event_1.to_json
+        it 'invoice.created event' do
+
+          post_request_body = {"id": "evt_00000000000001", "object": "event", "api_version": "2017-05-25", "created": 1326853478, "data": {"object": {"id": "in_1APVed2eZvKYlo2CP6dsoJTo", "object": "invoice", "amount_due": 599, "application_fee": nil, "attempt_count": 1, "attempted": true, "charge": "ch_1APVed2eZvKYlo2C1QNH6jru", "closed": true, "currency": "gbp", "customer": valid_trial_student.stripe_customer_id, "date": 1496232623, "description": nil, "discount": nil, "ending_balance": 0, "forgiven": false, "lines": {"data": [{"id": valid_subscription.stripe_guid, "object": "line_item", "amount": 599, "currency": "gbp", "description": nil, "discountable": true, "livemode": true, "metadata": {}, "period": {"start": 1498824623, "end": 1501503023}, "plan": {"id": subscription_plan_gbp_m.stripe_guid, "object": "plan", "amount": 999, "created": 1496222010, "currency": "gbp", "interval": "month", "interval_count": 1, "livemode": false, "metadata": {}, "name": subscription_plan_gbp_m.name, "statement_descriptor": nil, "trial_period_days": nil}, "proration": false, "quantity": 1, "subscription": nil, "subscription_item": "si_1APVed2eZvKYlo2C387JnMRE", "type": "subscription"}], "total_count": 1, "object": "list", "url": "/v1/invoices/in_1APVed2eZvKYlo2CP6dsoJTo/lines"}, "livemode": false, "metadata": {}, "next_payment_attempt": nil, "paid": true, "period_end": 1496232623, "period_start": 1496232623, "receipt_number": nil, "starting_balance": 0, "statement_descriptor": nil, "subscription": valid_subscription.stripe_guid, "subtotal": 599, "tax": nil, "tax_percent": nil, "total": 599, "webhooks_delivered_at": 1496232630}}, "livemode": false, "pending_webhooks": 1, "request": "req_Al1inviVvNH7df", "type": "invoice.created"}
+
+
+          url = 'https://api.stripe.com/v1/events/evt_00000000000001'
+          stub_event_get_request(url, post_request_body)
+
+          post :create, post_request_body.to_json
 
           expect(StripeApiEvent.count).to eq(1)
           sae = StripeApiEvent.last
           expect(sae.processed).to eq(true)
           expect(sae.error).to eq(false)
           expect(sae.error_message).to eq(nil)
-
           expect(Invoice.count).to eq(1)
           invoice = Invoice.last
-          expect(invoice.user_id).to eq(student.id)
-          expect(invoice.subscription_id).to eq(subscription_1.id)
-          expect(invoice.stripe_guid).to eq(invoice_created_event_1.data.object.id)
-          expect(InvoiceLineItem.count).to eq(invoice_created_event_1.data.object.lines.data.length)
+          expect(invoice.user_id).to eq(valid_trial_student.id)
+          expect(invoice.subscription_id).to eq(valid_subscription.id)
+          expect(invoice.stripe_guid).to eq('in_1APVed2eZvKYlo2CP6dsoJTo')
+          expect(InvoiceLineItem.count).to eq(1)
         end
 
         xit 'invoice.payment_failed first attempt' do
