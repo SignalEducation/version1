7430afe5bf6821b2eee97d39f2a10904051549fc
Aidan Quilligan <a.quilligan@gmail.com>
2018-11-08 12:24:36 +0000

    Fix #1154 Seting the start date to the correct point in the future.

--- app/services/paypal_service.rb
+++ app/services/paypal_service.rb
@@ -90,7 +90,7 @@ class PaypalService
     {
       name: subscription_plan.name,
       description: subscription_plan.description.gsub("\n", ""),
-      start_date: (Time.zone.now + 1.month).iso8601,
+      start_date: (Time.zone.now + subscription_plan.payment_frequency_in_months.months).iso8601,
       payer: {
         payment_method: "paypal",
         payer_info: {
