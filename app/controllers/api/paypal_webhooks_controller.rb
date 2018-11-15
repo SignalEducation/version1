9b8bf6dfb0380b25251daf7020dac1ad964a4e73
Aidan Quilligan <a.quilligan@gmail.com>
2018-11-15 12:14:42 +0000

    Paypal endpoint head status.

--- app/controllers/api/paypal_webhooks_controller.rb
+++ app/controllers/api/paypal_webhooks_controller.rb
@@ -5,13 +5,13 @@ class Api::PaypalWebhooksController < Api::BaseController
     @webhook = PaypalWebhooksService.new(request, params[:paypal].to_json)
     if @webhook.valid?
       @webhook.process
-      render head: 204
+      render body: nil, status: 204
     else
       Rails.logger.error "ERROR: Api/Paypal#Create: Unable to verify PayPal webhook: #{request.headers}"
-      render head: 404
+      render body: nil, status: 404
     end
   rescue => e
     Rails.logger.error "ERROR: Api/Paypal#Create: Unable to process PayPal webhook: #{request.headers}"
-    render head: 404
+    render body: nil, status: 404
   end
 end
