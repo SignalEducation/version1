4e27e8ecb2fe0533000e9dded615c003b449b042
Aidan Quilligan <a.quilligan@gmail.com>
2018-11-21 12:36:18 +0000

    Fixtures and stubs in place for PayPal REST API.

--- spec/services/paypal_service_spec.rb
+++ spec/services/paypal_service_spec.rb
@@ -2,6 +2,54 @@ require 'rails_helper'
 
 describe PaypalService, type: :service do
 
+  describe '#create_billing_agreement' do
+    let(:subscription) { build_stubbed(:subscription) }
+    let(:auth_response) { File.read('./spec/fixtures/paypal/auth_request_response.rb').to_json }
+    let(:billing_agreement) { File.read('./spec/fixtures/paypal/billing_agreement_response.rb').to_json }
+
+    before :each do
+      stub_request(:post, "https://api.sandbox.paypal.com/v1/oauth2/token").
+        with(
+          body: {"grant_type"=>"client_credentials"},
+          headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Authorization'=>'Basic UkFORE9NX0NMSUVOVF9JRDpSQU5ET01fQ0xJRU5UX1NFQ1JFVA==',
+            'Content-Type'=>'application/x-www-form-urlencoded',
+            'User-Agent'=>'PayPalSDK/PayPal-Ruby-SDK 1.7.2 (paypal-sdk-core 1.7.2; ruby 2.2.9p480-x86_64-darwin17;OpenSSL 1.0.2p  14 Aug 2018)'
+          }
+        ).to_return(
+          status: 200,
+          body: auth_response, 
+          headers: {}
+        )
+      stub_request(:post, "https://api.sandbox.paypal.com/v1/payments/billing-agreements/").
+        to_return(
+          status: 200,
+          body: billing_agreement, 
+          headers: {}
+        )
+    end
+
+    it 'calls the correct paypal REST API method' do
+      # expect_any_instance_of(Agreement).to receive(:create).and_return(true)
+
+      subject.send(:create_billing_agreement, subscription, subscription.user)
+    end
+  end
+
+  describe '#suspend_billing_agreement' do
+    it 'calls the correct paypal REST API method'
+  end
+
+  describe '#reactivate_billing_agreement' do
+    it 'calls the correct paypal REST API method'
+  end
+
+  describe '#cancel_billing_agreement' do
+    it 'calls the correct paypal REST API method'
+  end
+
   # PRIVATE METHODS ############################################################
 
   describe '#agreement_attributes' do
@@ -38,6 +86,45 @@ describe PaypalService, type: :service do
     end
   end
 
+  describe '#plan_attributes' do
+    let(:plan) { build_stubbed(:subscription_plan) }
+
+    it 'returns the correct hash' do
+      expect(subject.send(:plan_attributes, plan))
+        .to eq (
+          {
+            name: "LearnSignal #{plan.name}",
+            description: plan.description,
+            type: "INFINITE",
+            payment_definitions: [
+              {
+                name: "Regular payment definition",
+                type: "REGULAR",
+                frequency: "MONTH",
+                frequency_interval: plan.payment_frequency_in_months.to_s,
+                amount: {
+                  value: plan.price.to_s,
+                  currency: plan.currency.iso_code
+                },
+                cycles: "0",
+              }
+            ],
+            merchant_preferences: {
+              setup_fee: {
+                value: "0",
+                currency: plan.currency.iso_code
+              },
+              return_url: "https://example.com",
+              cancel_url: "https://example.com/cancel",
+              auto_bill_amount: "YES",
+              initial_fail_amount_action: "CANCEL",
+              max_fail_attempts: "4"
+            }
+          }
+        )
+    end
+  end
+
   # describe '#patch' do
   #   let(:)
   # end
@@ -53,45 +140,6 @@ describe PaypalService, type: :service do
   describe 'async methods that communicate with PayPal / Stripe' do
     before :each do
       allow_any_instance_of(SubscriptionPlanService).to receive(:queue_async).and_return(true)
-    end
-
-    describe '#plan_attributes' do
-      let(:plan) { create(:subscription_plan) }
-
-      it 'returns the correct hash' do
-        expect(subject.send(:plan_attributes, plan))
-          .to eq (
-            {
-              name: "LearnSignal #{plan.name}",
-              description: plan.description,
-              type: "INFINITE",
-              payment_definitions: [
-                {
-                  name: "Regular payment definition",
-                  type: "REGULAR",
-                  frequency: "MONTH",
-                  frequency_interval: plan.payment_frequency_in_months.to_s,
-                  amount: {
-                    value: plan.price.to_s,
-                    currency: plan.currency.iso_code
-                  },
-                  cycles: "0",
-                }
-              ],
-              merchant_preferences: {
-                setup_fee: {
-                  value: "0",
-                  currency: plan.currency.iso_code
-                },
-                return_url: "https://example.com",
-                cancel_url: "https://example.com/cancel",
-                auto_bill_amount: "YES",
-                initial_fail_amount_action: "CANCEL",
-                max_fail_attempts: "4"
-              }
-            }
-          )
-      end
     end
 
     describe '#update_subscription_plan' do
