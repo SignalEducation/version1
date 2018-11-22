1b22902e21ce1a24b756c6266d48042164aa9887
Aidan Quilligan <a.quilligan@gmail.com>
2018-11-22 12:00:49 +0000

    Stripe Service specs fleshed out.

--- spec/services/stripe_service_spec.rb
+++ spec/services/stripe_service_spec.rb
@@ -4,17 +4,54 @@ describe StripeService, type: :service do
 
   # INSTANCE METHODS ###########################################################
 
+  # CUSTOMERS ==================================================================
+
   describe '#create_customer' do
-    it 'does stuff'
+    let(:user) { create(:user) }
+
+    it 'creates the customer on Stripe' do
+      expect(Stripe::Customer).to receive(:create).and_return(double(id: 'stripe_test_id'))
+
+      subject.create_customer(user)
+    end
+
+    it 'updates the user with the Stripe customer id' do
+      allow(Stripe::Customer).to receive(:create).and_return(double(id: 'stripe_test_id'))
+
+      expect {
+        subject.create_customer(user)
+      }.to change { user.stripe_customer_id }.from(nil).to('stripe_test_id')
+    end
   end
 
   describe '#get_customer' do
-    it 'does stuff'
+    it 'calls #retrieve on Stripe::Customer with the passed in Stripe ID' do
+      expect(Stripe::Customer).to receive(:retrieve).with('stripe_test_id')
+
+      subject.get_customer('stripe_test_id')
+    end
   end
 
+  # PLANS ======================================================================
+
   describe '#create_plan' do
-    it 'does stuff'
+    let(:plan) { build_stubbed(:subscription_plan) }
+
+    it 'calls CREATE on Stripe::Plan with the correct args' do
+      expect(Stripe::Plan).to receive(:create)
+      allow(subject).to receive(:update_subscription_plan)
+
+      subject.create_plan(plan)
+    end
+
+    it 'calls #update_subscription_plan with the correct args' do
+      allow(Stripe::Plan).to receive(:create)
+      expect(subject).to receive(:update_subscription_plan)
+
+      subject.create_plan(plan)
+    end
   end
+
 
   # PRIVATE METHODS ############################################################
 end
