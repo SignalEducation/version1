6e98709c3769ce610de30faea225e2703656294c
Aidan Quilligan <a.quilligan@gmail.com>
2019-01-22 12:13:02 +0000

    Rails helper update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,7 @@ SimpleCov.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
-require File.expand_path('../../config/environment', __FILE__)
+require_relative '../config/environment'
 require 'rspec/rails'
 require 'factory_bot_rails'       # suggested by stack overflow
 #require 'rspec/autorun'
