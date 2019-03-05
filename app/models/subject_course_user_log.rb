52cca24fc9c0ad020e6d2a323b25805a2bc3c7d8
Aidan Quilligan <a.quilligan@gmail.com>
2019-03-05 19:10:29 +0000

    RecalculateCompleteness tweaks.

--- app/models/subject_course_user_log.rb
+++ app/models/subject_course_user_log.rb
@@ -90,17 +90,17 @@ class SubjectCourseUserLog < ActiveRecord::Base
 
   def recalculate_completeness
     # Temp fix replaced SET scope with_active_cmes with with_valid_course_module scope
-    self.count_of_questions_correct = self.student_exam_tracks.with_valid_course_module.sum(:count_of_questions_correct)
-    self.count_of_questions_taken = self.student_exam_tracks.with_valid_course_module.sum(:count_of_questions_taken)
-    self.count_of_videos_taken = self.student_exam_tracks.with_valid_course_module.sum(:count_of_videos_taken)
-    self.count_of_quizzes_taken = self.student_exam_tracks.with_valid_course_module.sum(:count_of_quizzes_taken)
-    self.count_of_constructed_responses_taken = self.student_exam_tracks.with_valid_course_module.sum(:count_of_constructed_responses_taken)
-    self.count_of_cmes_completed = self.student_exam_tracks.with_valid_course_module.sum(:count_of_cmes_completed)
-    self.percentage_complete = (self.count_of_cmes_completed.to_f / self.elements_total.to_f) * 100
-    unless self.percentage_complete.nil?
-      self.completed = true if (self.percentage_complete > 99)
-    end
-    self.save
+    # self.count_of_questions_correct = self.student_exam_tracks.with_valid_course_module.sum(:count_of_questions_correct)
+    # self.count_of_questions_taken = self.student_exam_tracks.with_valid_course_module.sum(:count_of_questions_taken)
+    # self.count_of_videos_taken = self.student_exam_tracks.with_valid_course_module.sum(:count_of_videos_taken)
+    # self.count_of_quizzes_taken = self.student_exam_tracks.with_valid_course_module.sum(:count_of_quizzes_taken)
+    # self.count_of_constructed_responses_taken = self.student_exam_tracks.with_valid_course_module.sum(:count_of_constructed_responses_taken)
+    # self.count_of_cmes_completed = self.student_exam_tracks.with_valid_course_module.sum(:count_of_cmes_completed)
+    # self.percentage_complete = (self.count_of_cmes_completed.to_f / self.elements_total.to_f) * 100
+    # unless self.percentage_complete.nil?
+    #   self.completed = true if (self.percentage_complete > 99)
+    # end
+    # self.save
   end
 
   def student_exam_track_course_module_ids
