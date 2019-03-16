65444918cf68da02eb9edacc2a2039938d22f13c
James MacRedmond <james@learnsignal.com>
2019-03-16 09:25:33 +0000

    Reverted some more fixes, since this branch is going straight to production

--- app/workers/stripe_api_processor_worker.rb
+++ app/workers/stripe_api_processor_worker.rb
@@ -4,6 +4,6 @@ class StripeApiProcessorWorker
   sidekiq_options queue: 'medium'
 
   def perform(stripe_event_id, api_version, account_url = "")
-    StripeApiEvent.find_or_create_by!(guid: stripe_event_id, api_version: api_version, account_url: account_url)
+    StripeApiEvent.create!(guid: stripe_event_id, api_version: api_version, account_url: account_url)
   end
 end
