ae397569ed3d38413cfe809a951c27c0a3cc8150
James MacRedmond <james@learnsignal.com>
2019-03-17 12:25:56 +0000

    First attempt to add new styles to video and quiz pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 // //// Removed Turbolinks from just after jquery-sortable
-//= require jquery3
+//= require jquery
 //= require ahoy
 //= require jquery_ujs
 //= require jquery-ui
