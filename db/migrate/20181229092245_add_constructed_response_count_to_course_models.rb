4f509acbbbf89b95930499a748075b7058279a54
James MacRedmond <james@learnsignal.com>
2019-03-26 09:40:05 +0000

    Fixed issue with pre-Rails-5 migration files not specifying the rails version

--- db/migrate/20181229092245_add_constructed_response_count_to_course_models.rb
+++ db/migrate/20181229092245_add_constructed_response_count_to_course_models.rb
@@ -1,4 +1,4 @@
-class AddConstructedResponseCountToCourseModels < ActiveRecord::Migration
+class AddConstructedResponseCountToCourseModels < ActiveRecord::Migration[4.2]
   def change
     add_column :course_modules, :constructed_response_count, :integer, default: 0
     add_column :course_sections, :constructed_response_count, :integer, default: 0
