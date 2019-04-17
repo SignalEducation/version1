3be58852aec81ab5313886b91762f698583de85a
Qamir Hussain <qhussain@aveclabs.com>
2019-04-17 11:27:39 +0100

    Refactored - seed data moved into separate files

---
+++ db/seed_data/user_countries.rb
@@ -0,0 +1,60 @@
+print 'Users: '
+
+  unless Rails.env.production?
+    generic_default_values = {
+            password: '123123123', password_confirmation: '123123123',
+            country_id: 1,
+            operational_email_frequency: 'daily',
+            study_plan_notifications_email_frequency: 'daily',
+            falling_behind_email_alert_frequency: 'daily',
+            marketing_email_frequency: 'daily',
+            blog_notification_email_frequency: 'daily',
+            forum_notification_email_frequency: 'daily',
+            locale: 'en'
+    }
+    User.where(id: 1).first_or_create!(generic_default_values.merge({
+            email: 'individual.student@example.com',
+            first_name: 'Individual',
+            last_name: 'Student',
+            user_group_id: 1
+    })); print '.'
+
+    User.where(id: 2).first_or_create!(generic_default_values.merge({
+            email: 'tutor@example.com',
+            first_name: 'Doctor',
+            last_name: 'Tutor',
+            user_group_id: 4
+    })); print '.'
+
+    User.where(id: 3).first_or_create!(generic_default_values.merge({
+            email: 'blogger@example.com',
+            first_name: 'Blogger',
+            last_name: 'Writer',
+            user_group_id: 5
+    })); print '.'
+
+    User.where(id: 4).first_or_create!(generic_default_values.merge({
+            email: 'forum.manager@example.com',
+            first_name: 'Forum',
+            last_name: 'Manager',
+            user_group_id: 6
+    })); print '.'
+
+    User.where(id: 5).first_or_create!(generic_default_values.merge({
+            email: 'content.manager@example.com',
+            first_name: 'Content',
+            last_name: 'Manager',
+            user_group_id: 7
+    })); print '.'
+
+    User.where(id: 6).first_or_create!(generic_default_values.merge({
+            email: 'site.admin@example.com',
+            first_name: 'Site',
+            last_name: 'Admin',
+            user_group_id: 8
+    })); print '.'
+
+    User.where(id: (1..6).to_a).update_all(active: true, account_activated_at: Time.now, account_activation_code: nil)
+  end
+
+  puts ' DONE'
