be49e60f705d90f9448a25395b122ddacde279bb
Qamir Hussain <qhussain@aveclabs.com>
2019-04-25 14:50:54 +0100

    formatting fixes

--- db/seed_data/database_maintenance.rb
+++ db/seed_data/database_maintenance.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ActiveRecord::Base.connection.tables.each do |t|
     ActiveRecord::Base.connection.reset_pk_sequence!(t)
-  end
+end
--- db/seed_data/user_group.rb
+++ db/seed_data/user_group.rb
@@ -1,34 +1,36 @@
+# frozen_string_literal: true
+
 UserGroup.where(id: 1).first_or_create!(
-    name: 'Individual students', description: 'Self-funded students'
+  name: 'Individual students', description: 'Self-funded students'
 ); print '.'
 
 UserGroup.where(id: 2).first_or_create!(
-    name: 'Corporate Student',
-    description: 'Student, funded by a corporate customer'
+  name: 'Corporate Student',
+  description: 'Student, funded by a corporate customer'
 ); print '.'
 
 UserGroup.where(id: 3).first_or_create!(
-    name: 'Corporate customers',
-    description: 'Administrative users on behalf of a corporate customer'
+  name: 'Corporate customers',
+  description: 'Administrative users on behalf of a corporate customer'
 ); print '.'
 
 UserGroup.where(id: 4).first_or_create!(
-    name: 'Tutor', description: 'Can create course content'
+  name: 'Tutor', description: 'Can create course content'
 ); print '.'
 
 UserGroup.where(id: 5).first_or_create!(
-    name: 'Blogger', description: 'Can create blog content'
+  name: 'Blogger', description: 'Can create blog content'
 ); print '.'
 
 UserGroup.where(id: 6).first_or_create!(
-    name: 'Content manager',
-    description: 'Can manage forum, blog and static pages'
+  name: 'Content manager',
+  description: 'Can manage forum, blog and static pages'
 ); print '.'
 
 UserGroup.where(id: 7).first_or_create!(
-    name: 'Admin', description: 'Can do everything'
+  name: 'Admin', description: 'Can do everything'
 ); print '.'
 
 UserGroup.where(id: 8).first_or_create!(
-    name: 'Complimentary users', description: 'Like a student, but free'
+  name: 'Complimentary users', description: 'Like a student, but free'
 ); print '.'
