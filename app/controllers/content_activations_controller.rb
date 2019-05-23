664dd6e103bdf1f8c639e67a60ec620c43a48767
Qamir Hussain <qhussain@aveclabs.com>
2019-05-23 14:24:43 +0100

    Code tidy

--- app/controllers/content_activations_controller.rb
+++ app/controllers/content_activations_controller.rb
@@ -11,23 +11,18 @@ class ContentActivationsController < ApplicationController
   end
 
   def create
-
-    
     record_type = params[:type].constantize
     @record = record_type.find(params[:id])
 
     # Defaults to 2AM for the selected date
     @date = DateTime.new(params[:activation_date]["date(1i)"].to_i,
-                       params[:activation_date]["date(2i)"].to_i,
-                       params[:activation_date]["date(3i)"].to_i, 
-                       2
-                      )    
-    
-    if @record && params[:activation_date]
-
-     #parsed_time = DateTime.strptime(datetime, '%d/%m/%Y %H:%M:%S')
+                          params[:activation_date]["date(2i)"].to_i,
+                          params[:activation_date]["date(3i)"].to_i, 
+                          2
+                        )    
+                        
+    if @record && params[:activation_date]     
       ContentActivationWorker.perform_at(@date, record_type, @record.id)
-
       redirect_to content_activation_special_link(@record)
     end
   end
