147017c620d9aab593e748a3b18bcdb1c870f5db
Aidan Quilligan <a.quilligan@gmail.com>
2019-05-23 12:08:27 +0100

    Ordering visits page properly.

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -34,5 +34,5 @@ class Visit < ActiveRecord::Base
   has_many :events, class_name: "Ahoy::Event"
   belongs_to :user, optional: true
 
-  scope :search_for, lambda { |search_term| where('utm_source ILIKE :t OR utm_medium ILIKE :t OR utm_term ILIKE :t  OR utm_content ILIKE :t  OR utm_campaign ILIKE :t ', t: '%' + search_term + '%') }
+  scope :search_for, lambda { |search_term| where('utm_source ILIKE :t OR utm_medium ILIKE :t OR utm_term ILIKE :t  OR utm_content ILIKE :t  OR utm_campaign ILIKE :t ', t: '%' + search_term + '%').order(started_at: :desc) }
 end
