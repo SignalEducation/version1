f6c5a50b653ac346606acfe476a5880b591147e6
Aidan Quilligan <a.quilligan@gmail.com>
2019-06-10 09:55:57 +0100

    Assigning a currency to a user on signup.

--- app/controllers/student_sign_ups_controller.rb
+++ app/controllers/student_sign_ups_controller.rb
@@ -83,11 +83,14 @@ class StudentSignUpsController < ApplicationController
   def create
     @navbar = false
     @footer = false
+    user_country = IpAddress.get_country(request.remote_ip, true)
+    user_currency = user_country.currency || Currency.find_by(iso_code: 'GBP')
 
     @user = User.new(
       student_allowed_params.merge(
         user_group: UserGroup.student_group,
-        country: IpAddress.get_country(request.remote_ip, true)
+        country: IpAddress.get_country(request.remote_ip, true),
+        currency: user_currency
       )
     )
     @user.pre_creation_setup
