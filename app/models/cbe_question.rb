81c7fd6404eb964230db780d3c6abab2f177cc43
qamir husssain <qhussain@aveclabs.com>
2019-06-25 18:08:37 +0100

    CBE Question

---
+++ app/models/cbe_question.rb
@@ -0,0 +1,2 @@
+class CbeQuestion < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_25_170140) do
+ActiveRecord::Schema.define(version: 2019_06_25_170734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -101,6 +101,13 @@ ActiveRecord::Schema.define(version: 2019_06_25_170140) do
     t.text "content"
     t.string "title"
     t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cbe_questions", force: :cascade do |t|
+    t.string "label"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
