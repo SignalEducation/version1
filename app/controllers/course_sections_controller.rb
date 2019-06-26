ea48cfbc76d5f174861a7408c453106257067f19
Giordano Alves <giordanofalves@gmail.com>
2019-06-26 09:47:19 +0100

    Doing some improves here.

--- app/controllers/course_sections_controller.rb
+++ app/controllers/course_sections_controller.rb
@@ -31,8 +31,8 @@ class CourseSectionsController < ApplicationController
 
   def new
     @subject_course = SubjectCourse.where(id: params[:id]).first
-    if @subject_course && @subject_course.course_sections.count > 0
-      @course_section = CourseSection.new(sorting_order: ((@subject_course.course_sections.any? && @subject_course.course_sections.all_in_order.last.sorting_order) ? @subject_course.course_sections.all_in_order.last.sorting_order + 1 : 1), subject_course_id: @subject_course.id)
+    if @subject_course&.course_sections.positive?
+      @course_section = CourseSection.new(sorting_order: (@subject_course.course_sections.any? && @subject_course.course_sections.all_in_order.last.sorting_order ? @subject_course.course_sections.all_in_order.last.sorting_order + 1 : 1), subject_course_id: @subject_course.id)
     elsif @subject_course
       @course_section = CourseSection.new(sorting_order: 1, subject_course_id: @subject_course.id)
     else
@@ -50,8 +50,7 @@ class CourseSectionsController < ApplicationController
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @course_section.update_attributes(allowed_params)
