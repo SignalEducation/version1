ddc1116d50e7b75496ed9a1adfefbcf8926e3f89
qamir husssain <qhussain@aveclabs.com>
2019-06-28 17:54:55 +0100

    WIP

--- spec/models/cbe_spec.rb
+++ spec/models/cbe_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Cbe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "When testing the CBE Model" do
+    it 'validates presence' do
+      cbe = Cbe.new
+      expect(cbe.name).to eq nil
+    end
+  end
 end
