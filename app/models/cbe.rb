f93a85ffe25d65c451d266957c8671767824190d
qamir husssain <qhussain@aveclabs.com>
2019-07-02 14:27:54 +0100

    rubocop fixes

--- app/models/cbe.rb
+++ app/models/cbe.rb
@@ -1,19 +1,19 @@
+# frozen_string_literal: true
+
 class Cbe < ApplicationRecord
-  has_many :cbe_sections
-  has_many :cbe_questions
-  has_many :cbe_introduction_pages
-  has_one :cbe_agreement
+  has_many :cbe_sections, dependent: :destroy
+  has_many :cbe_questions, dependent: :destroy
+  has_many :cbe_introduction_pages, dependent: :destroy
+  has_one :cbe_agreement, dependent: :destroy
 
   validates :name, :title, :description, :exam_body_id, presence: true
-
 
   def initialize_settings(exam_time = 120, number_of_pauses_allowed = 3, length_of_pauses = 15)
     self.exam_time = exam_time
     self.number_of_pauses_allowed = number_of_pauses_allowed
     self.length_of_pauses = length_of_pauses
     self.hard_time_limit = exam_time * 2
-    self.save
+    save
   end
-
 
 end
