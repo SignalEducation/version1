b1bbc11dc6cfec7684b029836fb6c75257c5107a
qamir husssain <qhussain@aveclabs.com>
2019-07-02 15:12:48 +0100

    WIP - CBE seeds data added custom migrate file added to db:seed

---
+++ db/seed_data/cbe_question_status.rb
@@ -0,0 +1,11 @@
+CbeQuestionStatus.first_or_create!(
+    name: 'Complete',
+); print '.'
+
+CbeQuestionStatus.first_or_create!(
+    name: 'Unseen',
+); print '.'
+
+CbeQuestionStatus.first_or_create!(
+    name: 'Incomplete',
+); print '.'
--- db/seed_data/cbe_section_type.rb
+++ db/seed_data/cbe_section_type.rb
@@ -1,11 +1,3 @@
-CbeQuestionType.first_or_create!(
-    name: 'Objective Question',
-); print '.'
-
-CbeQuestionType.first_or_create!(
-    name: 'Contrusted Response',
-); print '.'
-
-CbeQuestionType.first_or_create!(
-    name: 'Objective Test Case',
-); print '.'
+CbeQuestionType.first_or_create!(name: 'Objective Question'); print '.'
+CbeQuestionType.first_or_create!(name: 'Contructed Response'); print '.'
+CbeQuestionType.first_or_create!(name: 'Objective Test Case'); print '.'
