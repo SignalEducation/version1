6a43b604de7a5658721b26ae0e4b0c7f7495e6ee
qamir husssain <qhussain@aveclabs.com>
2019-07-02 13:39:01 +0100

    settings & validations added

--- app/models/cbe.rb
+++ app/models/cbe.rb
@@ -4,11 +4,16 @@ class Cbe < ApplicationRecord
   has_many :cbe_introduction_pages
   has_one :cbe_agreement
 
+  validates :name, :title, :description, :exam_body_id, presence: true
 
-  def initialize(name, title, description)
-    @name = name
-    @title = title
-    @description = description
+
+  def initialize_settings(exam_time = 120, number_of_pauses_allowed = 3, length_of_pauses = 15)
+    self.exam_time = exam_time
+    self.number_of_pauses_allowed = number_of_pauses_allowed
+    self.length_of_pauses = length_of_pauses
+    self.hard_time_limit = exam_time * 2
+    self.save
   end
+
 
 end
