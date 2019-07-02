f346493bc935857169dacbe3a1e5b5692df51ee6
qamir husssain <qhussain@aveclabs.com>
2019-07-02 17:21:32 +0100

    Tests added for default settings

--- spec/models/cbe_spec.rb
+++ spec/models/cbe_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Cbe, type: :model do
       expect(cbe.exam_body_id).to eq nil
     end
   end
-  
+
   context 'When creating a CBE with values set' do
     it 'Should create a new CBE record with those values saved' do
       cbe = Cbe.create(name: 'BCCA EXAM', title: 'exam 1', description: 'test desc', exam_body_id: 1)
@@ -23,6 +23,24 @@ RSpec.describe Cbe, type: :model do
     it 'Should return errors' do
       cbe = Cbe.create
       expect(cbe.errors.blank?).to eq false
+    end
+  end
+
+  context 'When creating a CBE with default settings' do
+    it 'Should create a new CBE record default settings' do
+      cbe = Cbe.create(name: 'BCCA EXAM', title: 'exam 1', description: 'test desc', exam_body_id: 1)
+      expect(cbe.hard_time_limit).to eq(nil)
+      expect(cbe.exam_time).to eq nil
+      expect(cbe.length_of_pauses).to eq nil
+      expect(cbe.number_of_pauses_allowed).to eq nil
+
+      cbe.initialize_settings
+      expect(cbe.hard_time_limit).to eq(240)
+      expect(cbe.exam_time).to eq 120
+      expect(cbe.length_of_pauses).to eq 15
+      expect(cbe.number_of_pauses_allowed).to eq 32
+
+      expect(cbe.errors.blank?).to eq true
     end
   end
 
