a63d83a4e9cf7d2fd72365c6f16db2fa2e5168fe
qamir husssain <qhussain@aveclabs.com>
2019-07-02 14:37:30 +0100

    Added cbe section type seed data

---
+++ db/seed_data/cbe_section_type.rb
@@ -0,0 +1,11 @@
+CbeQuestionType.first_or_create!(
+    name: 'Objective Question',
+); print '.'
+
+CbeQuestionType.first_or_create!(
+    name: 'Contrusted Response',
+); print '.'
+
+CbeQuestionType.first_or_create!(
+    name: 'Objective Test Case',
+); print '.'
