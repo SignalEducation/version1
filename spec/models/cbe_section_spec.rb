12c13892b8fe13a6ee622fce1b52b21670689d61
qamir husssain <qhussain@aveclabs.com>
2019-07-03 15:11:44 +0100

    RSpec fix

--- spec/models/cbe_section_spec.rb
+++ spec/models/cbe_section_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe CbeSection, type: :model do
   context 'When creating an empty CBE Section' do
     it 'Should return an error as it needs to be attached to a CBE' do
       cbe = Cbe.create(name: 'BCCA EXAM', title: 'exam 1', description: 'test desc', exam_body_id: 1)
-      cbe_section = CbeSection.create(name: 'Intro', scenario_label: 'S Label', scenario_description: 'S Desc', cbe: cbe)
+      cbe_section = CbeSection.create(name: 'Intro',
+                                      scenario_label: 'S Label',
+                                      scenario_description: 'S Desc',
+                                      cbe: cbe)
 
       expect(cbe_section.name).not_to eq nil
       expect(cbe_section.scenario_label).not_to eq nil
