0cbf2e7d27a826e4c10c38ddc5c8c4123f92a3e4
qamir husssain <qhussain@aveclabs.com>
2019-07-03 15:55:13 +0100

    RSpec test for cbe questions and muliple choice questions added

--- spec/models/cbe_question_spec.rb
+++ spec/models/cbe_question_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe CbeQuestion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  cbe = Cbe.create(name: 'BCCA EXAM', title: 'exam 1', description: 'test desc', exam_body_id: 1)
+  cbe_section = CbeSection.create(name: 'Intro')
+  cbe_queston = CbeMultipleChoiceQuestion.create()
 end
