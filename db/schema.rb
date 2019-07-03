fe2cc629bee7377483533a957980b64285e32032
qamir husssain <qhussain@aveclabs.com>
2019-07-03 14:11:11 +0100

    Refactored multiple choice question table

--- db/schema.rb
+++ db/schema.rb
@@ -109,6 +109,16 @@ ActiveRecord::Schema.define(version: 2019_07_14_093157) do
     t.index ["cbe_introduction_page_id"], name: "index_cbe_introduction_pages_on_cbe_introduction_page_id"
   end
 
+  create_table "cbe_multiple_choice_questions", force: :cascade do |t|
+    t.string "label"
+    t.boolean "is_correct_answer"
+    t.bigint "cbe_question_grouping_id"
+    t.integer "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cbe_question_grouping_id"], name: "index_cbe_multiple_choice_questions_on_cbe_question_grouping_id"
+  end
+
   create_table "cbe_question_groupings", force: :cascade do |t|
     t.bigint "cbe_id"
     t.bigint "cbe_section_id"
@@ -1387,6 +1397,7 @@ ActiveRecord::Schema.define(version: 2019_07_14_093157) do
 
   add_foreign_key "cbe_agreements", "cbe_agreements"
   add_foreign_key "cbe_introduction_pages", "cbe_introduction_pages"
+  add_foreign_key "cbe_multiple_choice_questions", "cbe_question_groupings"
   add_foreign_key "cbe_question_groupings", "cbe_sections"
   add_foreign_key "cbe_question_groupings", "cbes"
   add_foreign_key "cbe_question_types", "cbe_questions"
