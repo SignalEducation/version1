a9af41308ce6587ae2d77fe26602b284521d6312
qamir husssain <qhussain@aveclabs.com>
2019-07-04 15:02:39 +0100

    Create CBE button & method added to frontend

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <button v-on:click="createCBE">Create CBE</button>
+    <br/><br/>
+    <button v-on:click="creatSection">Create Section</button>
+    <br/><br/>
+    <button v-on:click="creatQuestion">Create Question</button>
   </div>
 </template>
 
@@ -8,11 +13,21 @@
 export default {
   data: function () {
     return {
-      message: "Hello Q"
+      message: "Create A CBE",
+      cbe_data: []
     }
-  }
+  },
+   methods: {
+          createCBE: function(page, index) {
+              console.log("this is working. Page:", page, '. Index:', index)
+          }
+      }
 }
 </script>
+
+
+
+
 
 <style scoped>
 p {
