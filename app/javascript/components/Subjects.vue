ce9beeae90d4893e9d7b47b369b39da5fb42d047
qamir husssain <qhussain@aveclabs.com>
2019-07-16 15:42:22 +0100

    Subjects is now it's own component

---
+++ app/javascript/components/Subjects.vue
@@ -0,0 +1,49 @@
+<template>           
+    <select v-model="selectedSubject">
+        <option>Choose Option</option>
+        <option v-for="option in options" v-bind:value="option.id">
+         {{option.name}}
+        </option>
+    </select>      
+</template>
+
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        mounted(){
+            this.showSubjects()
+        },
+        data: function () {
+            return {
+                selectedSubject: null,
+                options: []
+            }
+
+        },
+        methods: {
+            showSubjects: function (page, index) {
+                console.log('TEST 1')
+                axios.get('http://localhost:3000/cbes/1/get_subjects/')
+                    .then(response => {
+                        console.log('** Subjects loaded')
+                        this.options = response.data
+                        console.log(this.subjects)
+                    })
+                    .catch(e => {
+                        console.log('Error')
+                    })
+
+            },
+        }
+    }
+</script>
+
+
+<style scoped>
+    p {
+        font-size: 2em;
+        text-align: center;
+    }
+</style>
