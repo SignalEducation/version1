2018485023d8cbf4db1f3087f00d4b13bc52bfbe
qamir husssain <qhussain@aveclabs.com>
2019-07-18 14:20:01 +0100

    WIP Section component added

---
+++ app/javascript/components/CBEDetails.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>
+       
+        </p>
+        <input v-model="cbeTitle" placeholder="CBE Title">
+        <input v-model="cbeName" placeholder="CBE Name">
+        <input v-model="cbeDescription" placeholder="CBE Name">
+        <input v-model="cbeTimeLimit" placeholder="Exam Time Limit">
+        <input v-model="cbeNumberOfPausesAllowed" placeholder="Number of pauses allowed">
+        <input v-model="cbeLengthOfEachPause" placeholder="Length of each pause"> 
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
