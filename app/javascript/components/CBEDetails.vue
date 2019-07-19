553e7c4f171ed71e4b48b2cc336277923fa16bcb
qamir husssain <qhussain@aveclabs.com>
2019-07-19 10:28:20 +0100

    CBE Details are now as unordered list

--- app/javascript/components/CBEDetails.vue
+++ app/javascript/components/CBEDetails.vue
@@ -1,40 +1,43 @@
 <template>
-    <div>
-        <p>
-       
-        </p>
-         <fieldset>
-             <label>Title</label>
-            <input v-model="cbeTitle">
-         </fieldset>
-
-          <fieldset>
-             <label>Name</label>
-            <input v-model="cbeName">
-         </fieldset>
-
-        <fieldset>
-            <label>Description</label>
-            <input v-model="cbeDescription">
-         </fieldset>
-
-        <fieldset>
-            <label>Time Limit</label>
-            <input v-model="cbeTimeLimit">
-         </fieldset>
-
-        <fieldset>
-            <label>Number of allowable pauses</label>
-            <input v-model="cbeNumberOfPausesAllowed">
-         </fieldset>
-
-        <fieldset>
-            <label>Number of allowable pauses</label>
-            <input v-model="cbeLengthOfEachPause" >
-         </fieldset>   
-
-
-
+    <div>         
+         <ul id="cbe-details-list">
+            <li class="cbe-detail">        
+                <fieldset>
+                    <label>Title</label>
+                    <input v-model="cbeTitle">
+                </fieldset>
+            </li>
+             <li class="cbe-detail">
+                <fieldset>
+                    <label>Name</label>
+                    <input v-model="cbeName">
+                </fieldset>
+             </li>
+             <li class="cbe-detail">
+                <fieldset>
+                    <label>Description</label>
+                    <input v-model="cbeDescription">
+                </fieldset>                 
+             </li>
+             <li class="cbe-detail">
+                <fieldset>
+                    <label>Time Limit</label>
+                    <input v-model="cbeTimeLimit">
+                </fieldset>
+             </li>
+             <li class="cbe-detail">
+                <fieldset>
+                    <label>Number of allowable pauses</label>
+                    <input v-model="cbeNumberOfPausesAllowed">
+                </fieldset>                 
+             </li>
+             <li class="cbe-detail">
+                <fieldset>
+                    <label>Number of allowable pauses</label>
+                    <input v-model="cbeLengthOfEachPause" >
+                </fieldset>   
+             </li>
+         </ul>
     </div>
 </template>
 
