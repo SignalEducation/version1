35a1c6f50eb553c287f7177e2dc8281609d3ad17
Aidan Quilligan <a.quilligan@gmail.com>
2019-09-02 11:06:00 +0100

    UPDATE: SCA authentication handling for changing plan
    * Revert schema deletion
    * FIX: validations for changing subscription should only happen on: :create.
    * Javascript fixes for plan changes.
    * Handling the error path for failing 3DS.
    * Codeclimate fixes.
    * Spec fixes.
    * Displaying better errors for users on failed plan changes.
    * Code syntax cleanup - as per Giordanos comments.

--- app/controllers/subscriptions/plan_changes_controller.rb
+++ app/controllers/subscriptions/plan_changes_controller.rb
@@ -1,43 +1,66 @@
-class Subscriptions::PlanChangesController < ApplicationController
-  before_action :logged_in_required
-  before_action do
-    ensure_user_has_access_rights(%w(student_user))
-  end
-  before_action :get_subscription
+# frozen_string_literal: true
 
-  def show
-    Rails.logger.info "DataLayer Event: PlanChanges#show - Subscription: #{@subscription.id}, Revenue: #{@subscription.subscription_plan.price}, PlanName: #{@subscription.subscription_plan.name}, Brand: #{@subscription.subscription_plan.exam_body.name}" if @subscription
-  end
-
-  def new
-  end
-
-  def create
-    subscription_object = SubscriptionService.new(@subscription)
-    if @subscription = subscription_object.change_plan(plan_change_params[:subscription_plan_id].to_i)
-      if subscription_object.stripe?
-        @subscription.start
-        subscription_object.validate_referral
-        redirect_to subscriptions_plan_change_url(@subscription), notice: 'Your new plan is confirmed!'
-      elsif subscription_object.paypal?
-        redirect_to @subscription.paypal_approval_url
-      end
-    else
-      Rails.logger.error "ERROR: SubscriptionsController#update - something went wrong."
-      flash[:error] = I18n.t('controllers.subscriptions.update.flash.error')
+module Subscriptions
+  class PlanChangesController < ApplicationController
+    before_action :logged_in_required
+    before_action do
+      ensure_user_has_access_rights(%w(student_user))
     end
-  rescue Learnsignal::SubscriptionError => e
-    flash[:error] = e.message
-    redirect_to account_url(anchor: 'account-info')
-  end
+    before_action :get_subscription
 
-  private
+    def show
+      Rails.logger.info "DataLayer Event: PlanChanges#show - Subscription: #{@subscription.id}, Revenue: #{@subscription.subscription_plan.price}, PlanName: #{@subscription.subscription_plan.name}, Brand: #{@subscription.subscription_plan.exam_body.name}" if @subscription
+    end
 
-  def get_subscription
-    @subscription = Subscription.find_by_id(params[:id])
-  end
+    def new; end
 
-  def plan_change_params
-    params.require(:subscription).permit(:subscription_plan_id)
+    def create
+      send("change_#{@subscription.subscription_type}_subscription",
+           @subscription, plan_change_params[:subscription_plan_id].to_i)
+    rescue Learnsignal::SubscriptionError => e
+      if request.xhr?
+        render json: { subscription_id: @subscription&.id,
+                       error: e.message }, status: :bad_request
+      else
+        flash[:error] = e.message
+        redirect_to account_url(anchor: 'account-info')
+      end
+    end
+
+    def status_from_stripe
+      case params[:status]
+      when 'active', 'succeeded'
+        @subscription.start
+      end
+    end
+
+    private
+
+    def get_subscription
+      @subscription = Subscription.find_by_id(params[:id])
+    end
+
+    def change_stripe_subscription(subscription, plan_id)
+      @subscription, data = StripeSubscriptionService.new(subscription).
+                              change_plan(plan_id)
+
+      if data[:status] == :ok
+        render 'subscriptions/create'
+      else
+        render json: { subscription_id: @subscription.id,
+                       error: data[:error_message] }, status: data[:status]
+      end
+    end
+
+    def change_paypal_subscription(subscription, _params)
+      @subscription =
+        PaypalSubscriptionsService.new(subscription).change_plan(plan_id)
+
+      redirect_to @subscription.paypal_approval_url
+    end
+
+    def plan_change_params
+      params.require(:subscription).permit(:subscription_plan_id)
+    end
   end
 end
