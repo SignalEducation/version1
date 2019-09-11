36762583456c97d541ce025fe4ce5c1fa707fb2b
qamir husssain <qhussain@aveclabs.com>
2019-09-11 11:16:57 +0100

    Formatting fixes

--- app/javascript/Show.vue
+++ app/javascript/Show.vue
@@ -3,21 +3,52 @@
     <h1>CBE settings</h1>
     <label for="cbe-name">CBE</label>
     <div class="field">
-      <span class="field-value" v-show="!showField('name')" @click="focusField('name')">{{cbe.name}}</span>
-      <input v-model="cbe.name" v-show="showField('name')" id="cbe-name" type="text" class="field-value form-control" @focus="focusField('name')" @blur="blurField"/>
+      <span
+        class="field-value"
+        v-show="!showField('name')"
+        @click="focusField('name')"
+      >{{cbe.name}}</span>
+      <input
+        v-model="cbe.name"
+        v-show="showField('name')"
+        id="cbe-name"
+        type="text"
+        class="field-value form-control"
+        @focus="focusField('name')"
+        @blur="blurField"
+      />
     </div>
 
     <label for="cbe-id">CBE Section</label>
     <div class="field">
-      <span class="field-value" v-show="!showField('id')" @click="focusField('id')">{{cbe.id}}</span>
-      <input v-model="cbe.id" v-show="showField('id')" type="id" class="field-value form-control" @focus="focusField('id')" @blur="blurField"/>
+      <span
+        class="field-value"
+        v-show="!showField('id')"
+        @click="focusField('id')"
+      >{{cbe.id}}</span>
+      <input
+        v-model="cbe.id"
+        v-show="showField('id')"
+        type="id"
+        class="field-value form-control"
+        @focus="focusField('id')"
+        @blur="blurField"
+      />
 
       <span></span>
       <div class="row">
         <div class="col-sm-10">
           <div class="form-group">
-            <select v-model="selectedCbe" class="form-control custom-select">
-              <option class="col-md-8" v-for="option in options" v-bind:value="option.id">
+            <select
+              v-model="selectedCbe"
+              class="form-control custom-select"
+            >
+              <option
+                class="col-md-8"
+                v-for="option in options"
+                v-bind:value="option.id"
+                v-bind:key="option.id"
+              >
                 {{option.name}}
               </option>
             </select>
@@ -29,52 +60,53 @@
 </template>
 
 <script>
-  import splitPane from "vue-splitpane";
-  import axios from "axios";
+import axios from 'axios';
 
-  export default {
-    mounted() {
-      this.fetchCbes();
+export default {
+  mounted() {
+    this.fetchCbes();
+  },
+  data() {
+    return {
+      cbe: {
+        name: 'CBE 1',
+        id: '1',
+      },
+      editField: '',
+      options: [],
+      selectedCbe: '',
+    };
+  },
+  methods: {
+    focusField(name) {
+      this.editField = name;
     },
-    data() {
-      return {
-        cbe: {
-          name: "CBE 1",
-          id: "1"
-        },
-        editField: "",
-        options: [],
-        selectedCbe: ""
-      };
+    blurField() {
+      this.editField = '';
     },
-    methods: {
-      focusField(name) {
-        this.editField = name;
-      },
-      blurField() {
-        this.editField = "";
-      },
-      showField(name) {
-        return this.cbe[name] == "" || this.editField == name;
-      },
-      fetchCbes: function(page, index) {
-        axios
-          .get("http://localhost:3000/api/v1/cbes/")
-          .then(response => {
-            // this.$store.questionTypes = response.data
-            this.options = response.data;
-          })
-          .catch(e => {
-            console.log("Error" + e);
-          });
-      }
-    }
-  };
+    showField(name) {
+      return this.cbe[name] === '' || this.editField === name;
+    },
+    // eslint-disable-next-line no-unused-vars
+    fetchCbes(page, index) {
+      axios
+        .get('http://localhost:3000/api/v1/cbes/')
+        .then((response) => {
+          // this.$store.questionTypes = response.data
+          this.options = response.data;
+        })
+        .catch((e) => {
+          // eslint-disable-next-line no-console
+          console.log(`Error${e}`);
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
-  p {
-    font-size: 2em;
-    text-align: center;
-  }
+p {
+  font-size: 2em;
+  text-align: center;
+}
 </style>
--- app/javascript/components/CBEQuestion.vue
+++ app/javascript/components/CBEQuestion.vue
@@ -3,9 +3,17 @@
     <div class="col-sm-6">
       <div class="form-group">
         <label for="questionKindSelect">Question Type</label>
-        <b-form-select v-model="questionKind" :options="questionKinds" id="questionKindSelect" class="input-group input-group-lg">
+        <b-form-select
+          v-model="questionKind"
+          :options="questionKinds"
+          id="questionKindSelect"
+          class="input-group input-group-lg"
+        >
           <template slot="first">
-            <option :value="null" disabled>-- Please select a type --</option>
+            <option
+              :value="null"
+              disabled
+            >-- Please select a type --</option>
           </template>
         </b-form-select>
       </div>
@@ -13,79 +21,99 @@
     <div class="col-sm-6">
       <div class="form-group">
         <label for="questionScore">Score</label>
-        <div class="input-group input-group-lg" id="questionScore">
-          <input v-model="questionScore" placeholder="Score" class="form-control"/>
+        <div
+          class="input-group input-group-lg"
+          id="questionScore"
+        >
+          <input
+            v-model="questionScore"
+            placeholder="Score"
+            class="form-control"
+          />
         </div>
       </div>
     </div>
     <div class="col-sm-12">
       <div class="form-group">
         <label for="questionContent">Content</label>
-        <div class="input-group input-group-lg" id="questionContent">
-          <textarea v-model="questionContent" placeholder="Content" class="form-control"></textarea>
+        <div
+          class="input-group input-group-lg"
+          id="questionContent"
+        >
+          <textarea
+            v-model="questionContent"
+            placeholder="Content"
+            class="form-control"
+          ></textarea>
         </div>
       </div>
     </div>
 
     <div class="form-group">
-      <button v-on:click="saveQuestion" class="btn btn-primary">Save Question</button>
+      <button
+        v-on:click="saveQuestion"
+        class="btn btn-primary"
+      >Save Question</button>
     </div>
 
   </div>
 </template>
 
 <script>
-  import axios from "axios";
+import axios from 'axios';
 
-  export default {
-    props: ['sectionId', 'scenarioId'],
-    data: function() {
-      return {
-        questionDetails: {},
-        questionKind: null,
-        questionContent: null,
-        questionScore: null,
-        selectedSelectQuestion: null,
-        questionKinds: [
-          'multiple_choice',
-          'multiple_response',
-          'complete',
-          'fill_in_the_blank',
-          'drag_drop',
-          'dropdown_list',
-          'hot_spot',
-          'spreadsheet',
-          'open'
-        ]
-      };
+export default {
+  props: ['sectionId', 'scenarioId'],
+  data() {
+    return {
+      questionDetails: {},
+      questionKind: null,
+      questionContent: null,
+      questionScore: null,
+      selectedSelectQuestion: null,
+      questionKinds: [
+        'multiple_choice',
+        'multiple_response',
+        'complete',
+        'fill_in_the_blank',
+        'drag_drop',
+        'dropdown_list',
+        'hot_spot',
+        'spreadsheet',
+        'open',
+      ],
+    };
+  },
+  methods: {
+    // eslint-disable-next-line no-unused-vars
+    saveQuestion(page, index) {
+      this.questionDetails = {};
+      this.questionDetails.kind = this.questionKind;
+      this.questionDetails.content = this.questionContent;
+      this.questionDetails.score = this.questionScore;
+      this.questionDetails.cbe_section_id = this.sectionId;
+      this.questionDetails.cbe_scenario_id = this.scenarioId;
+      // eslint-disable-next-line no-console
+      console.log(this.questionDetails);
+
+      axios
+        .post('/api/v1/cbe/questions/', {
+          question: this.questionDetails,
+        })
+        .then((response) => {
+          this.createdQuestion = response.data;
+          this.questionDetails.id = this.createdQuestion.id;
+          this.$emit('add-question', this.questionDetails);
+          this.questionDetails = {};
+          this.questionKind = '';
+          this.questionContent = '';
+          this.questionScore = '';
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.log(error);
+        });
     },
-    methods: {
-      saveQuestion: function(page, index) {
-        this.questionDetails = {};
-        this.questionDetails['kind'] = this.questionKind;
-        this.questionDetails['content'] = this.questionContent;
-        this.questionDetails['score'] = this.questionScore;
-        this.questionDetails['cbe_section_id'] = this.sectionId;
-        this.questionDetails['cbe_scenario_id'] = this.scenarioId;
-        console.log(this.questionDetails);
-
-        axios
-          .post("/api/v1/cbe/questions/", {
-            question: this.questionDetails
-          })
-          .then(response => {
-            this.createdQuestion = response.data;
-            this.questionDetails["id"] = this.createdQuestion.id;
-            this.$emit('add-question', this.questionDetails);
-            this.questionDetails = {};
-            this.questionKind = '';
-            this.questionContent = '';
-            this.questionScore = '';
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }
-    }
-  };
+  },
+};
 </script>
