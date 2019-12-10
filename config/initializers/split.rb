8c6c3427753c2252e7a96cd82b387b7ee38c9065
James MacRedmond <jmacredmond@gmail.com>
2019-12-10 12:33:37 +0000

    Added config for split gem to allow connection with redis

---
+++ config/initializers/split.rb
@@ -0,0 +1,7 @@
+Split.configure do |config|
+  config.db_failover = true # handle Redis errors gracefully
+  config.db_failover_on_db_error = -> (error) { Rails.logger.error(error.message) }
+  config.allow_multiple_experiments = true
+  config.enabled = true
+  config.redis = "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}/5"
+end
