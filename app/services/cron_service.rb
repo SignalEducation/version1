23232def0077bb31687db04c033437c56edd5909
Aidan Quilligan <a.quilligan@gmail.com>
2020-02-15 19:20:36 +0000

    FEATURE: Paypal syncing service
    * Moving cron tasks to the API namespace.
    * Paypal syncing tasks and moved cron_tasks_controller to api only.
    * Cron schedule setup.
    * Extra specs.

--- app/services/cron_service.rb
+++ app/services/cron_service.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CronService
-  TASK_LIST = %w[paypal_sync].freeze
+  TASK_LIST = %w[paypal_sync ping].freeze
 
   def initiate_task(task_name)
     raise(StandardError, "Cron Task #{task_name} is not defined") unless TASK_LIST.include?(task_name)
@@ -9,7 +9,12 @@ class CronService
     send(task_name)
   end
 
+  def ping
+    Rails.logger.info 'CRON: I just got a PING'
+  end
+
   def paypal_sync
     Rails.logger.info "CRON: Called the 'paypal_sync' task"
+    Paypal::SubscriptionValidation.run_paypal_sync
   end
 end
