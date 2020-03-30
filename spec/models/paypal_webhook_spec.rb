7acf681c19584f0b7f8907b321a50f4e0b1096a3
Aidan Quilligan <a.quilligan@gmail.com>
2020-03-30 13:59:53 +0100

    REFACTOR: Specs added for SubscriptionPlanWorker
    * Added additional specs for PaypalWebhook model.
    * StripeWebhooksController specs.
    * Specs for PaypalWebhookReprocessWorker
    * Added more specs for Paypal::SubscriptionValidation

--- spec/models/paypal_webhook_spec.rb
+++ spec/models/paypal_webhook_spec.rb
@@ -27,18 +27,15 @@ describe PaypalWebhook do
   # callbacks
   it { should callback(:check_dependencies).before(:destroy) }
 
-  # scopes
-
-  # instance methods
-  it { should respond_to(:destroyable?) }
-  it { should respond_to(:process_sale_completed) }
-  it { should respond_to(:process_sale_denied) }
-  it { should respond_to(:process_subscription_cancelled) }
-  it { should respond_to(:reprocess) }
-
   describe 'instance methods' do
     let(:paypal_webhook) { create(:paypal_webhook) }
     let(:subscription) { build_stubbed(:subscription) }
+
+    describe '#destroyable?' do
+      it 'returns TRUE' do
+        expect(paypal_webhook.destroyable?).to be_truthy
+      end
+    end
 
     describe '#verify_subscription' do
       it 'sets #verified to TRUE if a subscription exists' do
@@ -47,6 +44,7 @@ describe PaypalWebhook do
 
         paypal_webhook.verify_subscription('test')
       end
+
       it 'sets #verified to FALSE if a subscription does not exist' do
         allow(Subscription).to receive(:find_by).and_return(nil)
 
@@ -54,7 +52,127 @@ describe PaypalWebhook do
       end
     end
 
-    describe 'process_subscription_cancelled' do
+    describe '#process_sale_completed' do
+      describe 'for invalid invoices' do
+        before :each do
+          allow(Invoice).to receive(:build_from_paypal_data).and_return(nil)
+        end
+
+        it 'marks the paypal_webhook record as verified: false' do
+          expect(paypal_webhook).to receive(:update!).with(verified: false)
+
+          paypal_webhook.process_sale_completed
+        end
+      end
+
+      describe 'for valid invoices' do
+        let(:invoice) { create(:invoice) }
+
+        before :each do
+          allow_any_instance_of(SubscriptionPlanService).to receive(:queue_async)
+          allow(Invoice).to receive(:build_from_paypal_data).and_return(invoice)
+        end
+
+        it 'updates the relevant invoice' do
+          expect(invoice).to receive(:update!).with(paid: true, payment_closed: true)
+
+          paypal_webhook.process_sale_completed
+        end
+
+        it 'calls update_next_billing_date on the PaypalSubscriptionsService' do
+          expect_any_instance_of(PaypalSubscriptionsService).to receive(:update_next_billing_date)
+
+          paypal_webhook.process_sale_completed
+        end
+
+        it 'sets the processed_at timestamp' do
+          Timecop.freeze(Time.zone.now) do
+            expect(paypal_webhook).to receive(:update!).with(processed_at: Time.zone.now)
+
+            paypal_webhook.process_sale_completed
+          end
+        end
+
+        describe 'for active PayPal subscriptions' do
+          before :each do
+            @sub_double = double(paypal_status: 'Active', paypal_subscription_guid: nil)
+            allow(invoice).to receive(:subscription).and_return(@sub_double)
+          end
+
+          it 'does not update the subscription object' do
+            expect(@sub_double).not_to receive(:update!)
+
+            paypal_webhook.process_sale_completed
+          end
+        end
+
+        describe 'for in-active PayPal subscriptions' do
+          before :each do
+            @sub_double = double(paypal_status: 'Pending', paypal_subscription_guid: nil)
+            allow(invoice).to receive(:subscription).and_return(@sub_double)
+          end
+
+          it 'updates the paypal_status of the subscription to Active' do
+            expect(@sub_double).to receive(:update!).with(paypal_status: 'Active')
+
+            paypal_webhook.process_sale_completed
+          end
+        end
+      end
+    end
+
+    describe '#process_sale_denied' do
+      describe 'for invalid invoices' do
+        before :each do
+          allow(Invoice).to receive(:build_from_paypal_data).and_return(nil)
+        end
+
+        it 'marks the paypal_webhook record as verified: false' do
+          expect(paypal_webhook).to receive(:update!).with(verified: false)
+
+          paypal_webhook.process_sale_denied
+        end
+      end
+
+      describe 'for valid invoices' do
+        let(:invoice) { create(:invoice) }
+
+        before :each do
+          allow_any_instance_of(SubscriptionPlanService).to receive(:queue_async)
+          allow(Invoice).to receive(:build_from_paypal_data).and_return(invoice)
+        end
+
+        it 'updates the relevant invoice' do
+          expect(invoice).to receive(:update!).with(paid: false, payment_closed: false)
+
+          paypal_webhook.process_sale_denied
+        end
+
+        it 'increments the invoice attempt_count' do
+          expect(invoice).to receive(:increment!).with(:attempt_count)
+
+          paypal_webhook.process_sale_denied
+        end
+
+        it 'sets the processed_at timestamp' do
+          Timecop.freeze(Time.zone.now) do
+            expect(paypal_webhook).to receive(:update!).with(processed_at: Time.zone.now)
+
+            paypal_webhook.process_sale_denied
+          end
+        end
+
+        it 'records an error on the subscription object' do
+          sub_double = double('Subscription')
+          allow(Subscription).to receive(:find_by).and_return(sub_double)
+          expect(sub_double).to receive(:record_error!)
+
+          paypal_webhook.process_sale_denied
+        end
+      end
+    end
+
+    describe '#process_subscription_cancelled' do
       it 'calls #verify_subscription' do
         expect(paypal_webhook).to receive(:verify_subscription)
 
@@ -62,11 +180,19 @@ describe PaypalWebhook do
       end
     end
 
-    describe 'process_subscription_suspended' do
+    describe '#process_subscription_suspended' do
       it 'calls #verify_subscription' do
         expect(paypal_webhook).to receive(:verify_subscription)
 
         paypal_webhook.process_subscription_suspended
+      end
+    end
+
+    describe '#reprocess' do
+      it 'queues a job in the PaypalWebhookReprocessWorker' do
+        expect(PaypalWebhookReprocessWorker).to receive(:perform_async).with(paypal_webhook.id)
+
+        paypal_webhook.reprocess
       end
     end
   end
