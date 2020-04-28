ab0872cd58fc7e3064c03a53eb9c9a00d5766e9e
Giordano Alves <giordanofalves@gmail.com>
2020-04-28 14:03:16 +0100

    Library controller tests.

--- spec/controllers/library_controller_spec.rb
+++ spec/controllers/library_controller_spec.rb
@@ -1,45 +1,50 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe LibraryController, type: :controller do
-  let!(:gbp) { FactoryBot.create(:gbp) }
-  let!(:uk) { FactoryBot.create(:uk, currency_id: gbp.id) }
-  let!(:exam_body_1) { FactoryBot.create(:exam_body) }
-  let!(:group_1) { FactoryBot.create(:group, exam_body_id: exam_body_1.id) }
-  let!(:group_2) { FactoryBot.create(:group, exam_body_id: exam_body_1.id) }
-  let!(:subject_course_1)  { FactoryBot.create(:active_subject_course,
-                                               group_id: group_1.id,
-                                               exam_body_id: exam_body_1.id) }
-  let!(:subject_course_2)  { FactoryBot.create(:active_subject_course,
-                                               group_id: group_1.id,
-                                               computer_based: true,
-                                               exam_body_id: exam_body_1.id) }
+  let!(:gbp)               { create(:gbp) }
+  let!(:uk)                { create(:uk, currency_id: gbp.id) }
+  let!(:exam_body_1)       { create(:exam_body) }
+  let!(:group_1)           { create(:group, exam_body_id: exam_body_1.id) }
+  let!(:group_2)           { create(:group, exam_body_id: exam_body_1.id) }
+  let!(:subject_course_1)  { create(:active_subject_course,
+                                    group_id: group_1.id,
+                                    exam_body_id: exam_body_1.id) }
+  let!(:subject_course_2)  { create(:active_subject_course,
+                                    group_id: group_1.id,
+                                    computer_based: true,
+                                    exam_body_id: exam_body_1.id) }
 
-  let!(:mock_exam_1) { FactoryBot.create(:mock_exam, subject_course_id: subject_course_1.id) }
-  let!(:product_1) { FactoryBot.create(:product, mock_exam_id: mock_exam_1.id, currency_id: gbp.id, group_id: group_1.id) }
+  let!(:mock_exam_1)       { create(:mock_exam, subject_course_id: subject_course_1.id) }
+  let!(:product_1)         { create(:product, mock_exam_id: mock_exam_1.id, currency_id: gbp.id, group_id: group_1.id) }
 
-  context 'Not logged in: ' do
-    describe 'GET index' do
-      it 'renders the index view as 2 groups are active' do
-        get :index
-        expect(flash[:success]).to be_nil
-        expect(flash[:error]).to be_nil
-        expect(response.status).to eq(200)
-        expect(response).to render_template(:index)
-        expect(Group.count).to eq(2)
-      end
+  let!(:user)              { create(:user) }
+  let!(:course_log)        { create(:subject_course_user_log, subject_course_id: subject_course_1.id, user_id: user.id) }
 
-      it 'redirects to render to group_show as 1 group is active' do
-        group_2.update_attribute(:active, false)
-        get :index
-        expect(flash[:success]).to be_nil
-        expect(flash[:error]).to be_nil
-        expect(response.status).to eq(302)
-        expect(response).to redirect_to(library_group_url(group_name_url: group_1.name_url))
-        expect(Group.all_active.count).to eq(1)
-      end
+  describe 'GET index' do
+    it 'renders the index view as 2 groups are active' do
+      get :index
+      expect(flash[:success]).to be_nil
+      expect(flash[:error]).to be_nil
+      expect(response.status).to eq(200)
+      expect(response).to render_template(:index)
+      expect(Group.count).to eq(2)
     end
 
-    describe 'GET group_show' do
+    it 'redirects to render to group_show as 1 group is active' do
+      group_2.update_attribute(:active, false)
+      get :index
+      expect(flash[:success]).to be_nil
+      expect(flash[:error]).to be_nil
+      expect(response.status).to eq(302)
+      expect(response).to redirect_to(library_group_url(group_name_url: group_1.name_url))
+      expect(Group.all_active.count).to eq(1)
+    end
+  end
+
+  describe 'GET group_show' do
+    context 'with group' do
       it 'returns http success' do
         get :group_show, params: { group_name_url: group_1.name_url }
         expect(response).to have_http_status(:success)
@@ -52,8 +57,34 @@ RSpec.describe LibraryController, type: :controller do
       end
     end
 
-    describe 'GET course_show' do
-      it 'returns http success' do
+    context 'without group' do
+      it 'redirect to root url' do
+        get :group_show, params: { group_name_url: 'any/url/here' }
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(root_url)
+      end
+    end
+  end
+
+  describe 'GET course_show' do
+    context 'logged user' do
+      it 'has_sittings present' do
+        allow(controller).to receive(:current_user).and_return(user)
+        allow_any_instance_of(ExamBody).to receive(:has_sittings).and_return(true)
+
+        get :course_show, params: { subject_course_name_url: subject_course_1.name_url, group_name_url: group_1.name_url }
+        expect(response).to have_http_status(:success)
+        expect(flash[:success]).to be_nil
+        expect(flash[:error]).to be_nil
+        expect(response.status).to eq(200)
+        expect(response).to render_template(:course_show)
+        expect(Group.all_active.count).to eq(2)
+        expect(SubjectCourse.count).to eq(2)
+      end
+
+      it 'has_sittings ausent' do
+        allow(controller).to receive(:current_user).and_return(build(:user))
+
         get :course_show, params: { subject_course_name_url: subject_course_1.name_url, group_name_url: group_1.name_url }
         expect(response).to have_http_status(:success)
         expect(flash[:success]).to be_nil
@@ -65,15 +96,30 @@ RSpec.describe LibraryController, type: :controller do
       end
     end
 
-    describe 'POST user_contact_form' do
-      it 'redirect and return a success message' do
-        expect(Zendesk::RequestWorker).to receive(:perform_async).and_return(true)
+    context 'not logged user' do
+      it 'returns course_preview' do
+        allow_any_instance_of(SubjectCourse).to receive(:preview).and_return(true)
 
-        post :user_contact_form
-
-        expect(flash[:success]).to eq('Thank you! Your submission was successful. We will contact you shortly.')
-        expect(response.status).to eq(302)
+        get :course_show, params: { subject_course_name_url: subject_course_1.name_url, group_name_url: group_1.name_url }
+        expect(response).to have_http_status(:success)
+        expect(flash[:success]).to be_nil
+        expect(flash[:error]).to be_nil
+        expect(response.status).to eq(200)
+        expect(response).to render_template(:course_preview)
+        expect(Group.all_active.count).to eq(2)
+        expect(SubjectCourse.count).to eq(2)
       end
+    end
+  end
+
+  describe 'POST user_contact_form' do
+    it 'redirect and return a success message' do
+      expect(Zendesk::RequestWorker).to receive(:perform_async).and_return(true)
+
+      post :user_contact_form
+
+      expect(flash[:success]).to eq('Thank you! Your submission was successful. We will contact you shortly.')
+      expect(response.status).to eq(302)
     end
   end
 end
