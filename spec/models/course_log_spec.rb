2ac72a7ade2b94a9128f59ab5f6ecc8c861ab245
Giordano Alves <giordanofalves@gmail.com>
2020-06-03 15:04:14 +0100

    (Test) CourseLog model.

--- spec/models/course_log_spec.rb
+++ spec/models/course_log_spec.rb
@@ -1,28 +1,10 @@
-# == Schema Information
-#
-# Table name: course_logs
-#
-#  id                                   :integer          not null, primary key
-#  user_id                              :integer
-#  session_guid                         :string
-#  course_id                    :integer
-#  percentage_complete                  :integer          default("0")
-#  count_of_cmes_completed              :integer          default("0")
-#  latest_course_step_id      :integer
-#  completed                            :boolean          default("false")
-#  created_at                           :datetime         not null
-#  updated_at                           :datetime         not null
-#  count_of_questions_correct           :integer
-#  count_of_questions_taken             :integer
-#  count_of_videos_taken                :integer
-#  count_of_quizzes_taken               :integer
-#  completed_at                         :datetime
-#  count_of_constructed_responses_taken :integer
-#
-
+# frozen_string_literal: true
 require 'rails_helper'
 
 describe CourseLog do
+  let(:user)        { create(:user) }
+  let(:course)      { create(:active_course) }
+  let!(:course_log) { create(:course_log, course: course, user: user) }
 
   describe 'relationships' do
     it { should belong_to(:user) }
@@ -69,4 +51,64 @@ describe CourseLog do
     it { should respond_to(:completion_cme_count) }
   end
 
+  describe 'Methods' do
+    describe '.to_csv' do
+      context 'return course' do
+        it do
+          expect(CourseLog.to_csv).to include(
+            'id', 'user_id', 'user_email', 'f_name', 'l_name', 'enrolled', 'enrollment_sitting',
+            'exam_date', 'student_number', 'date_of_birth', 'completed', 'percentage_complete',
+            'count_of_cmes_completed', 'completion_cme_count'
+          )
+        end
+      end
+    end
+
+    describe '#destroyable?' do
+      context 'always return true' do
+        it { expect(course_log).to be_destroyable }
+      end
+    end
+
+    describe '#elements_total_for_completion' do
+      context 'zero as completation' do
+        it { expect(course_log.elements_total_for_completion).to be_zero }
+      end
+    end
+
+    describe '#recalculate_scul_completeness' do
+      subject { course_log.recalculate_scul_completeness }
+
+      context 'change completed lessons in log' do
+        it { expect { subject }.to change { course_log.count_of_videos_taken }.from(nil).to(0) }
+        it { expect { subject }.to change { course_log.count_of_notes_completed }.from(nil).to(0) }
+        it { expect { subject }.to change { course_log.count_of_constructed_responses_taken }.from(nil).to(0) }
+        it { expect(course_log.count_of_cmes_completed).to be_zero }
+      end
+    end
+
+    describe '#check_dependencies' do
+      it 'stubb destroyable to tru to cover method' do
+        allow_any_instance_of(CourseLog).to receive(:destroyable?).and_return(false)
+        course_log.destroy
+
+        expect(course_log.errors).not_to be_empty
+      end
+    end
+
+    describe '#emit_certificate' do
+      let(:worker) { CourseLogsWorker }
+
+      before do
+        Sidekiq::Testing.fake!
+        Sidekiq::Worker.clear_all
+        allow_any_instance_of(CourseLogsWorker).to receive(:perform).and_return(true)
+        allow_any_instance_of(Course).to receive(:emit_certificate?).and_return(true)
+      end
+
+      it 'work receive a job' do
+        expect { course_log.update(completed: true) }.to change(worker.jobs, :size).by(1)
+      end
+    end
+  end
 end
