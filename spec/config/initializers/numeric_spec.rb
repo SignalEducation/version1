365a0bec42aa78c81aa5506fc896fbffb0718a4b
Aidan Quilligan <a.quilligan@gmail.com>
2020-06-29 16:25:41 +0100

    SPEC: Multiple spec updates.
    * Added Webdrivers for consistent chromedriver handling and not requiring people to keep drivers in sync on local machines.
    * User password reset feature specs
    * Order purchase specs
    * Exercise submission process specs.
    * FIX: Rspec test results upload.
    * Artifact storage fix.
    * FIX: Screenshots for capybara.
    * Capybara screenshot updates.
    * Remove upload of coverage as it goes to simplecov.
    * Fix for file permissions for test results.
    * FIX: Stripe spec failing.:
    * SPEC: User registers while purchasing a product.
    * Fix: remove stripe call.

--- spec/config/initializers/numeric_spec.rb
+++ spec/config/initializers/numeric_spec.rb
@@ -6,6 +6,6 @@ describe Numeric, type: :initializer do
   let(:num) { rand(1..99) }
 
   describe '.percent_of' do
-    it { expect(num.percent_of(100)).to eq(num) }
+    it { expect(num.percent_of(100).round).to eq(num) }
   end
 end
