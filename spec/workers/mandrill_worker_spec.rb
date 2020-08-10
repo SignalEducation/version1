5556b260bfe8828a137bc12f5cc22e2a7ae2fa90
Aidan Quilligan <a.quilligan@gmail.com>
2020-08-10 19:04:13 +0100

    AIRBRAKE: Mandrill error fix (with retry mechanism)
    * FIX: codeclimate updates.
    * FIX: failing specs.
    * Code coverage update.
    * turn off annoying js specs.

--- spec/workers/mandrill_worker_spec.rb
+++ spec/workers/mandrill_worker_spec.rb
@@ -4,12 +4,11 @@ require 'rails_helper'
 require 'sidekiq/testing'
 
 RSpec.describe MandrillWorker do
-  let(:message)         { build(:message) }
+  let(:message)         { create(:message) }
   let(:response_params) { [{"email"=>"test-user@example.com", "status"=>"sent", "_id"=>"123456789abcdefg", "reject_reason"=>nil}] }
-  before do
+  before :each do
     Sidekiq::Testing.fake!
     Sidekiq::Worker.clear_all
-    allow_any_instance_of(MandrillClient).to receive(:send).and_return(response_params)
     allow(Message).to receive(:find).and_return(message)
   end
 
@@ -20,6 +19,28 @@ RSpec.describe MandrillWorker do
   subject { MandrillWorker }
 
   it 'Mandrill job is processed in high queue.' do
+    allow_any_instance_of(MandrillClient).to receive(:send).and_return(response_params)
     expect { subject.perform_at(message.process_at, message.id) }.to change(subject.jobs, :size).by(1)
+  end
+
+  it 'calls #send_message' do
+    expect_any_instance_of(MandrillWorker).to receive(:send_message)
+
+    subject.new.perform(message.id)
+  end
+
+  describe '#send_message' do
+    it 'calls the MandrillClient' do
+      expect_any_instance_of(MandrillClient).to receive(:send).and_return(response_params)
+
+      subject.new.send_message(message, 'send_set_password_email', 'url')
+    end
+
+    it 'updates the message' do
+      allow_any_instance_of(MandrillClient).to receive(:send).and_return(response_params)
+      expect(message).to receive(:update)
+
+      subject.new.send_message(message, 'send_set_password_email', 'url')
+    end
   end
 end
