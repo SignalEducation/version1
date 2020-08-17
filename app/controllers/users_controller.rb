bf21b656afe8a4ac2ff61f3ca44f1b70e7b5ff12
dsbonnelly <paparazzo3500@gmail.com>
2020-08-17 14:42:38 +0100

    restyled tutors index and show page, added contact url for tutors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -181,8 +181,8 @@ class UsersController < ApplicationController
 
   def allowed_params
     params.require(:user).permit(:email, :first_name, :last_name, :user_group_id, :address, :country_id,
-                                 :profile_image, :date_of_birth, :description, :student_number, :name_url,
-                                 :stripe_account_balance, :preferred_exam_body_id, :currency_id,
+                                 :profile_image, :date_of_birth, :description, :student_number, :name_url, 
+                                 :tutor_link, :stripe_account_balance, :preferred_exam_body_id, :currency_id,
                                  onboarding_process_attributes: [:id, :active])
   end
 
