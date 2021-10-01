f0bc82c26d8809aa64b18c33792729aa8aaa1dc6
James MacRedmond <jmacredmond@gmail.com>
2021-10-01 11:19:16 +0200

    Added routing back in to accept the en locals

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -171,7 +171,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options(options = {})
-    { locale: nil }
+    { :locale => I18n.locale == I18n.default_locale ? nil : I18n.locale  }
   end
 
   #### Session GUIDs and user tracking
